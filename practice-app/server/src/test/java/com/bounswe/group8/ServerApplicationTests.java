06e141a563e6856a8fe0cc8e464282bd508ac6f3
bahadirgezer <bahadirgezerbg@gmail.com>
2023-04-30 00:36:51 +0300

    created practice app spring project

---
+++ practice-app/server/src/test/java/com/bounswe/group8/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bounswe.group8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
