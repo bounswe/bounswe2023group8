76184fb9cf651b11a131fba99801b0700b281149
bahadirgezer <bahadirgezerbg@gmail.com>
2023-04-30 03:53:11 +0300

    backend: add controller layer files for template models

---
+++ practice-app/server/src/main/java/com/bounswe/group8/controller/PostController.java
@@ -0,0 +1,168 @@
+package com.bounswe.group8.controller;
+
+import com.bounswe.group8.payload.PostCreateRequest;
+import com.bounswe.group8.payload.dto.PostDto;
+import com.bounswe.group8.service.PostService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PostController {
+
+    final PostService postService;
+
+    /*
+     *  WARN: This endpoint is for testing purposes only.
+     */
+    @GetMapping("/all")
+    public ResponseEntity<List<PostDto>> getAllPosts() {
+        return ResponseEntity.ok(
+                postService.getAll()
+        );
+    }
+
+    /**
+     * P1: Gets posts with pagination.
+     *
+     * @param page      Page number. Starts at 0. Is the only required parameter.
+     * @param size      Number of elements per page. Default value is 10.
+     * @param sortBy    Field to sort by. Default value is "id".
+     * @param desc      Sort direction. Default value is "false".
+     * @return          List of posts in the page
+     */
+    @GetMapping
+    public ResponseEntity<List<PostDto>> getPostPage(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10", required = false) Integer size,
+            @RequestParam(defaultValue = "id", required = false) String sortBy,
+            @RequestParam(defaultValue = "false", required = false) Boolean desc) {
+
+        List<PostDto> postPage = postService.getPostPage(page, size, sortBy, desc);
+
+        if (postPage.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok().body(postPage);
+    }
+
+    /**
+     * P2: Get a post by id.
+     *
+     * @param id    Post id
+     * @return      PostDto
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(
+            @PathVariable Long id) {
+
+        PostDto postDto = postService.getPostById(id);
+
+        if (postDto == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().body(postDto);
+    }
+
+    /**
+     * P3-1: Create a post.
+     *
+     * @param postCreateRequest     create request
+     * @return                      PostDto - created post
+     */
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(
+            @RequestBody PostCreateRequest postCreateRequest) {
+
+        Long postId = postService.createPost(postCreateRequest);
+
+        if (postId == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(postId)
+                        .toUri())
+                .build();
+    }
+
+    /**
+     * P3-2: Create a post.
+     *
+     * @param title     title of the post
+     * @param content   content of the post
+     * @param userId    id of the user who creates the post
+     * @return          PostDto - created post
+     */
+    @PostMapping("/user/{userId}")
+    public ResponseEntity<PostDto> createPost(
+            @RequestParam String title,
+            @RequestParam String content,
+            @PathVariable Long userId){
+
+        Long postId = postService.createPost(title, content, userId);
+
+        if (postId == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(postId)
+                        .toUri())
+                .build();
+    }
+
+    /**
+     * P4: Update a post.
+     *
+     * @param id        id of the post to be updated
+     * @param title     new title of the post
+     * @param content   new content of the post
+     * @return          PostDto - updated post
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(
+            @PathVariable Long id,
+            @RequestParam String title,
+            @RequestParam String content) {
+
+        PostDto postDto = postService.updatePostById(id, title, content);
+
+        if (postDto == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().body(postDto);
+    }
+
+    /**
+     * P5: Delete a post.
+     *
+     * @param id    id of the post to be deleted
+     * @return      PostDto - deleted post
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PostDto> deletePost(
+            @PathVariable Long id) {
+
+        PostDto postDto = postService.deletePostById(id);
+
+        if (postDto == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().body(postDto);
+    }
+
+}
---
+++ practice-app/server/src/main/java/com/bounswe/group8/controller/UserController.java
@@ -0,0 +1,154 @@
+package com.bounswe.group8.controller;
+
+
+import com.bounswe.group8.payload.UserCreateRequest;
+import com.bounswe.group8.payload.dto.UserDto;
+import com.bounswe.group8.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserController {
+
+    final UserService userService;
+
+    /*
+     *  WARN: This is a dummy endpoint. It is not used in the project. Just for test purposes.
+     */
+    @GetMapping("/all")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(
+                userService.getAll()
+        );
+    }
+
+    /**
+     * U1: Gets users with pagination.
+     *
+     * @param page      Page number. Starts at 0. Is the only required parameter.
+     * @param size      Number of elements per page. Default value is 10.
+     * @param sortBy    Field to sort by. Default value is "id".
+     * @param desc      Sort direction. Default value is "false".
+     * @return          List of users in the page
+     */
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getUserPage(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10", required = false) Integer size,
+            @RequestParam(defaultValue = "id", required = false) String sortBy,
+            @RequestParam(defaultValue = "false", required = false) Boolean desc) {
+
+        List<UserDto> userPage = userService.getUserPage(page, size, sortBy, desc);
+
+        if (userPage.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok().body(userPage);
+    }
+
+    /**
+     * U2-1: Get a user by id.
+     *
+     * @param id    User id
+     * @return      UserDto
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+
+        UserDto userDto = userService.getUserById(id);
+
+        if (userDto == null)
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok().body(userDto);
+    }
+
+    /**
+     * U2-2: Get a user by username.
+     *
+     * @param username  User username
+     * @return          UserDto
+     */
+    @GetMapping("/username")
+    public ResponseEntity<UserDto> getUserByUsername(@RequestParam String username) {
+
+        UserDto userDto = userService.getUserByUsername(username);
+
+        if (userDto == null)
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok().body(userDto);
+    }
+
+    /**
+     * U3: Create a user.
+     * @param userCreateRequest    create request
+     * @return                     UserDto - created user
+     */
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(
+            @RequestBody UserCreateRequest userCreateRequest) {
+
+        Long userId = userService.createUser(userCreateRequest);
+
+        if (userId == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(userId)
+                        .toUri()
+        ).build();
+    }
+
+    /**
+     * U4: Update a user.
+     *
+     * @param id        id of the user to be updated.
+     * @param username  new username
+     * @param password  new password
+     * @return          UserDto - updated user
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(
+            @PathVariable Long id,
+            @RequestParam String username,
+            @RequestParam String password) {
+
+        UserDto userDto = userService.updateUserById(id, username, password);
+
+        if (userDto == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().body(userDto);
+    }
+
+    /**
+     * U5: Delete an user.
+     *
+     * @param id    id of the user to be deleted.
+     * @return      UserDto - deleted user
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserDto> deleteUser(@PathVariable Long id) {
+
+        UserDto userDto = userService.deleteUserById(id);
+
+        if (userDto == null)
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().body(userDto);
+    }
+
+}
