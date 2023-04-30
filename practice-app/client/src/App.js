91aab94a6750171230ccfcee1ebec529894e26ec
bakikucukcakiroglu <baki.kucukcakiroglu@gmail.com>
2023-04-30 13:45:23 +0300

    Change initial page.

--- practice-app/client/src/App.js
+++ practice-app/client/src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Practice App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
