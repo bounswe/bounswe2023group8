0a989d4c36f30f9cd1bca3a77793976dc8d27202
bakikucukcakiroglu <baki.kucukcakiroglu@gmail.com>
2023-05-06 22:31:35 +0300

    A request to wikidata has been implemented.

--- practice-app/client/src/views/Baki.js
+++ practice-app/client/src/views/Baki.js
@@ -1,13 +1,33 @@
+import React, {useState} from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
-import React from 'react';
+import TextField from '@mui/material/TextField';
+
 
 function Baki() {
-  return (
-    <div>
-        Baki
-        {/* <Autocomplete> </Autocomplete> */}
-    </div>
-  );
+
+    const [options, setOptions] = useState([]);
+
+    const handleInputChange = async (event, value) => {
+        // You can send a request to your API here and update the options state accordingly
+        const response = await fetch(`https://www.wikidata.org/w/api.php?action=wbsearchentities&format=json&search=${value}&language=en&uselang=en&type=item)`, {mode: 'no-cors', method: "post",
+            headers: {
+                 "Content-Type": "application/json"
+            },
+           });
+        const data = await response.json();
+        console.log("data", data);
+        setOptions(data.search.map((item) => ({label: item.id, value: item.id})));
+    };
+    return (
+        <div>
+            <Autocomplete
+                options={options}
+                getOptionLabel={(option) => option.label}
+                renderInput={(params) => <TextField {...params} label="Search" variant="outlined" />}
+                onInputChange={handleInputChange}
+                />
+        </div>
+    );
 }
 
 export default Baki;
