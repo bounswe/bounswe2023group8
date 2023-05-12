1e184b6ef18e6f0f3da644a18a3abe827abe312f
bahadirgezer <bahadirgezerbg@gmail.com>
2023-05-12 16:17:41 +0300

    add PortfolioUserRole enum for security

--- practice-app/server/src/main/java/com/bounswe/group8/utils/enums/PortfolioUserRole.java
+++ practice-app/server/src/main/java/com/bounswe/group8/utils/enums/PortfolioUserRole.java
@@ -1,2 +1,26 @@
-package com.bounswe.group8.utils.enums;public enum PortfolioUserRole {
+package com.bounswe.group8.utils.enums;
+
+public enum PortfolioUserRole {
+
+    OWNER("owner"),
+
+    EDITOR("editor"),
+
+    VIEWER("viewer");
+
+    private final String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    PortfolioUserRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return role;
+    }
+
 }
