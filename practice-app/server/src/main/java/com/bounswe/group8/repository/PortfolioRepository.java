8be360bac05205428b01d52e8bdd398271e9adde
bahadirgezer <bahadirgezerbg@gmail.com>
2023-05-12 20:45:58 +0300

    delete portfolio feature from Bahadir project

--- practice-app/server/src/main/java/com/bounswe/group8/repository/PortfolioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bounswe.group8.repository;
-
-import com.bounswe.group8.model.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-
-}
--- practice-app/server/src/main/java/com/bounswe/group8/repository/PortfolioUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bounswe.group8.repository;
-
-import com.bounswe.group8.model.PortfolioUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioUserRepository extends JpaRepository<PortfolioUser, Long> {
-
-}
