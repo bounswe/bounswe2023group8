96437d78a75b33420bbbf73500630294abe703a6
bahadirgezer <bahadirgezerbg@gmail.com>
2023-05-12 16:35:36 +0300

    add initial service classes for portfolio,
    portfoliouser and stock

--- practice-app/server/src/main/java/com/bounswe/group8/service/PortfolioUserService.java
+++ practice-app/server/src/main/java/com/bounswe/group8/service/PortfolioUserService.java
@@ -1,2 +1,20 @@
-package com.bounswe.group8.service;public class PortfolioUserService {
+package com.bounswe.group8.service;
+
+import com.bounswe.group8.model.PortfolioUser;
+import com.bounswe.group8.repository.PortfolioUserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PortfolioUserService {
+
+    final PortfolioUserRepository portfolioUserRepository;
+
+
 }
--- practice-app/server/src/main/java/com/bounswe/group8/service/StockService.java
+++ practice-app/server/src/main/java/com/bounswe/group8/service/StockService.java
@@ -1,2 +1,19 @@
-package com.bounswe.group8.service;public class StockService {
+package com.bounswe.group8.service;
+
+import com.bounswe.group8.model.Stock;
+import com.bounswe.group8.repository.StockRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class StockService {
+
+    final StockRepository stockRepository;
+
 }
