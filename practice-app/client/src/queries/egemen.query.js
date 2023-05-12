5fcd61c60fc8934feba1ce3e2725606d2f401da6
egemenkaplan <egemen.kapln@gmail.com>
2023-05-12 19:58:22 +0300

    Last touches for swagger documentation

--- practice-app/client/src/queries/egemen.query.js
+++ practice-app/client/src/queries/egemen.query.js
@@ -6,11 +6,10 @@ import config from "../config";
  *
  * @param latitude                  {number}
  * @param longitude                 {number}
- * @param googleMapsApiKey          {string}
  * @returns {Promise<string[]>}     List of addresses as formatted strings
  */
-export const fetchAddresses = async (latitude, longitude, googleMapsApiKey) => {
-  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleMapsApiKey}`)
+export const fetchAddresses = async (latitude, longitude) => {
+  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`)
     .then(response => response.json());
   return [...new Set(response.results.map(element => element.formatted_address))];
 };
--- practice-app/server/src/main/java/com/bounswe/group8/controller/LocationController.java
+++ practice-app/server/src/main/java/com/bounswe/group8/controller/LocationController.java
@@ -3,6 +3,10 @@ package com.bounswe.group8.controller;
 import com.bounswe.group8.payload.LocationCreateRequest;
 import com.bounswe.group8.payload.dto.LocationDto;
 import com.bounswe.group8.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -21,13 +25,19 @@ public class LocationController {
     final LocationService locationService;
 
     /**
-     * Creates a new address in the database under a given title.
-     * 30 addresses per title limit.
+     * Creates a new location in the database under a given title.
+     * 10 addresses per title limit.
      *
      * @param locationCreateRequest     Create request
      * @return                          Created location
      */
     @PostMapping
+    @Operation(
+            summary = "Post a new location.",
+            description = "Create a new location in the database. There is a 10 location per title limit.",
+            responses = {
+                    @ApiResponse(description = "Added location")
+            })
     public ResponseEntity<LocationDto> createLocation(
             @RequestBody LocationCreateRequest locationCreateRequest) {
 
@@ -55,6 +65,12 @@ public class LocationController {
      * @return      List of locations
      */
     @GetMapping("/{title}")
+    @Operation(
+            summary = "Get locations.",
+            description = "Get locations saved under a title.",
+            responses = {
+                    @ApiResponse(description = "List of locations")
+            })
     public ResponseEntity<List<LocationDto>> getLocationsByTitle(
             @PathVariable String title
     ) {
@@ -67,6 +83,12 @@ public class LocationController {
      * @return          Number of locations
      */
     @GetMapping("/{title}/count")
+    @Operation(
+            summary = "Get count of locations",
+            description = "Get the count of locations saved under a title.",
+            responses = {
+                    @ApiResponse(description = "Count")
+            })
     public ResponseEntity<Integer> getCountByTitle(
             @PathVariable String title) {
 
