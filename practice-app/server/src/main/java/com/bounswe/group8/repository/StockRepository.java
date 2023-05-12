632f75a8093aa35cf7db7a7bbe60a54f0ed9eab5
bahadirgezer <bahadirgezerbg@gmail.com>
2023-05-12 16:35:06 +0300

    add repository and model for stocks

---
+++ practice-app/server/src/main/java/com/bounswe/group8/repository/StockRepository.java
@@ -0,0 +1,8 @@
+package com.bounswe.group8.repository;
+
+import com.bounswe.group8.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<Stock, Long> {
+
+}
---
+++ practice-app/server/src/main/java/com/bounswe/group8/service/PortfolioService.java
@@ -0,0 +1,2 @@
+package com.bounswe.group8.service;public class PortfolioService {
+}
