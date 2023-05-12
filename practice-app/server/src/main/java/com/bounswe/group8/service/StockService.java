94a061c615f748bd398b5d98e27bec7f570a48fa
Bahadır Gezer <bahadirgezerbg@gmail.com>
2023-05-12 20:56:59 +0300

    Merge pull request #147 from bounswe/PA-144
    [PA] Add Backend for Portfolio Management Section
    # Conflicts:
    #	practice-app/server/src/main/java/com/bounswe/group8/controller/StockController.java
    #	practice-app/server/src/main/java/com/bounswe/group8/controller/WikiController.java
    #	practice-app/server/src/main/java/com/bounswe/group8/service/StockService.java
    #	practice-app/server/src/main/resources/application.properties

--- practice-app/server/src/main/java/com/bounswe/group8/service/StockService.java
+++ practice-app/server/src/main/java/com/bounswe/group8/service/StockService.java
@@ -1,23 +1,35 @@
 package com.bounswe.group8.service;
 
-import com.bounswe.group8.model.StarStock;
-import com.bounswe.group8.model.User;
-import com.bounswe.group8.payload.AddStarRequest;
-import com.bounswe.group8.payload.StockPriceResponse;
-import com.bounswe.group8.payload.dto.StarStockDto;
-import com.bounswe.group8.payload.dto.StockPriceDto;
-import com.bounswe.group8.repository.StarStockRepository;
-import com.bounswe.group8.repository.UserRepository;
+import com.bounswe.group8.exception.custom.ApiRequestException;
+import com.bounswe.group8.exception.custom.ResourceNotFoundException;
+import com.bounswe.group8.model.Stock;
+import com.bounswe.group8.model.StockInfo;
+import com.bounswe.group8.model.StockSearchKeyword;
+import com.bounswe.group8.payload.dto.StockAutocompleteInfoDto;
+import com.bounswe.group8.payload.dto.StockDto;
+import com.bounswe.group8.payload.response.AVGlobalQuoteResponse;
+import com.bounswe.group8.payload.response.AVSymbolSearchResponse;
+import com.bounswe.group8.projections.StockAutocompleteProjection;
+import com.bounswe.group8.repository.StockInfoRepository;
+import com.bounswe.group8.repository.StockRepository;
+import com.bounswe.group8.repository.StockSearchKeywordRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Arrays;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -26,66 +38,256 @@ import java.util.stream.Collectors;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StockService {
 
-    final StarStockRepository starStockRepository;
+    final StockRepository stockRepository;
 
-    final UserRepository userRepository;
+    final StockInfoRepository stockInfoRepository;
 
-    private String polygonApiKey = "zU5yH0bOuEGYnRBpqCxeRiuAAljoU_Rr";
-    final String baseUrl = "https://api.polygon.io/v2/aggs/ticker/";
+    final StockSearchKeywordRepository stockSearchKeywordRepository;
 
+    @Value("${api.alpha-vantage.base-url}")
+    String baseUrl;
 
-    public StockPriceDto getStockPrice(String stock) {
+    @Value("${api.alpha-vantage.api-key}")
+    String apiKey;
 
-        String url = baseUrl + stock + "/range/1/day/2023-01-09/2023-01-09?apiKey=" + polygonApiKey;
+    /**
+     * Fetches the latest stock data from Alpha Vantage API.
+     * Checks the database for the latest stock data for the given symbol.
+     * If the latest stock data is not before today, returns the latest stock data from database.
+     * If the latest stock data is before today, fetches the latest stock data from Alpha Vantage API,
+     * saves it to database and returns it.
+     *
+     * @param symbol Stock symbol
+     * @return       StockDto
+     */
+    public StockDto findLatestStockBySymbol(String symbol) {
+
+        /* Get stock data from database */
+
+        Stock stock = stockRepository.findFirstStockBySymbolOrderByTradingDayDesc(symbol);
+
+        if (stock != null)
+            if (!isBeforeToday(stock.getTradingDay()))
+                return StockDto.convertToStockDto(stock);
+
+        /*  Get updated stock data */
+
+        Stock updatedStock = getUpdatedStockData(symbol);
+
+        /* Save updated stock data to database */
+
+        stockRepository.save(updatedStock);
+
+        return StockDto.convertToStockDto(updatedStock);
+    }
+
+    /**
+     * Fetches any stock data for a given symbol, regardless of the date.
+     *
+     * @param symbol Stock symbol
+     * @return       StockDto
+     */
+    public StockDto findStockBySymbol(String symbol) {
+
+        Stock stock = stockRepository.findFirstStockBySymbolOrderByTradingDayDesc(symbol);
+
+        if (stock != null)
+            return StockDto.convertToStockDto(stock);
+
+        /*  Get updated stock data if not found in database */
+
+        Stock updatedStock = getUpdatedStockData(symbol);
+
+        /* Save updated stock data to database */
+
+        stockRepository.save(updatedStock);
+
+        return StockDto.convertToStockDto(updatedStock);
+    }
+
+    /**
+     * Finds all the stock data for symbols containing the keyword parameter.
+     *
+     * @param keyword   Stock symbol
+     * @return          Map of (Symbol, List of StockDto)
+     */
+    public Map<String, List<StockDto>> searchStockData(String keyword) {
+
+        Set<Stock> stockList = stockRepository.findAllBySymbolContainingIgnoreCase(keyword);
+
+        if (stockList == null || stockList.isEmpty())
+            return Map.of();
+
+        return stockList.stream()
+                .map(stock -> {
+                        StockDto stockDto = StockDto.convertToStockDto(stock);
+                        stockDto.setId(stock.getId());
+                        return stockDto;
+                })
+                .collect(Collectors.groupingBy(StockDto::getSymbol));
+    }
+
+    /**
+     * Fetches all stock data from database.
+     *
+     * @return       List of StockDto
+     */
+    public List<StockDto> findEveryStock() {
+
+            List<Stock> stockList = stockRepository.findAll();
+
+            if (stockList.isEmpty())
+                return null;
+
+            return stockList.stream()
+                    .map(stock -> {
+                            StockDto stockDto = StockDto.convertToStockDto(stock);
+                            stockDto.setId(stock.getId());
+                            return stockDto;
+                    })
+                    .collect(Collectors.toList());
+    }
+
+    /**
+     * Auto-completes the keyword parameter with stock symbols.
+     *
+     * @param keyword  Stock symbol
+     * @return         StockInfoDto
+     */
+    public List<StockAutocompleteInfoDto> autoComplete(String keyword) {
+
+        List<StockAutocompleteInfoDto> stockInfoList = getStockAutocompleteInfoFromDatabase(keyword);
+
+        if (!stockInfoList.isEmpty())
+            return stockInfoList;
+
+        Set<StockInfo> stockInfoListFromApi = getStockAutocompleteInfo(keyword);
+
+        Set<StockInfo> stockInfoSet = stockInfoRepository.findAllBySymbolIsIn(
+                stockInfoListFromApi.stream()
+                        .map(StockInfo::getSymbol)
+                        .collect(Collectors.toSet()));
+
+        stockInfoListFromApi.removeAll(stockInfoSet);
+        List<StockInfo> savedStockInfoList = stockInfoRepository.saveAll(stockInfoListFromApi);
+
+        stockSearchKeywordRepository.saveAll(
+                savedStockInfoList.stream()
+                        .map(stockInfo -> new StockSearchKeyword(
+                                null,
+                                keyword,
+                                stockInfo.getId()))
+                        .collect(Collectors.toList())
+        );
+
+        return stockInfoListFromApi.stream()
+                .map(stock -> new StockAutocompleteInfoDto(
+                        stock.getSymbol(),
+                        stock.getName()))
+                .collect(Collectors.toList());
+    }
+
+    private Set<StockInfo> getStockAutocompleteInfo(String keyword) {
+
+        log.info("Getting autocomplete data for keyword: {} from Alpha Vantage API", keyword);
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .queryParam("function", "SYMBOL_SEARCH")
+                .queryParam("keywords", keyword)
+                .queryParam("apikey", apiKey);
+
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<StockPriceResponse[]> responseEntity;
-        try {
-            responseEntity = restTemplate.getForEntity(url, StockPriceResponse[].class);
-        } catch (Exception e) {
-            log.error("Error while getting stock price", e);
-            return null;
+        ResponseEntity<AVSymbolSearchResponse> response = restTemplate
+                .exchange(uriBuilder.toUriString(), HttpMethod.GET, null, AVSymbolSearchResponse.class);
+
+        if (response.getStatusCode().isError()) {
+            log.error("Could not get autocomplete data for keyword: {}", keyword);
+            throw new ApiRequestException("Could not get autocomplete data for keyword: " +
+                    keyword, response.getStatusCode().value());
         }
 
-        if (!responseEntity.getStatusCode().is2xxSuccessful())
-            return null;
+        if (response.getBody() == null) {
+            log.error("Autocomplete data is null for keyword: {}", keyword);
+            throw new ApiRequestException("Autocomplete data is null for keyword: " + keyword, 404);
+        }
 
-        StockPriceResponse[] stockResponses = responseEntity.getBody();
-        assert stockResponses != null;
-        return new StockPriceDto()
-                .setStatus(responseEntity.getStatusCode().value())
-                .setStocks(
-                        Arrays.stream(stockResponses)
-                                .map(stockResponse -> new StockPriceDto.Stock()
-                                        .setStock(stockResponse.getStock())
-                                        .setH(stockResponse.getH())
-                                )
-                                .collect(Collectors.toList())
-                );
+        List<AVSymbolSearchResponse.BestMatch> bestMatches = response.getBody().getBestMatches();
+        return bestMatches.stream()
+                .map(match -> new StockInfo(
+                        null,
+                        match.getSymbol(),
+                        match.getName(),
+                        match.getType(),
+                        match.getRegion(),
+                        match.getCurrency()
+                    )
+                ).collect(Collectors.toSet());
     }
 
-    public List<StarStockDto> getStarStocks(Long userId){
-        List<StarStock> starStocks = starStockRepository.findAllByUserId(userId);
+    private List<StockAutocompleteInfoDto> getStockAutocompleteInfoFromDatabase(String keyword) {
 
-        return starStocks.stream()
-                .map(starStock -> new StarStockDto()
-                        .setStock(starStock.getStock())
-                        .setPrice(starStock.getPrice())
+        log.info("Getting autocomplete data for keyword: {} from database", keyword);
+
+        List<StockAutocompleteProjection> stockInfoList =
+                stockSearchKeywordRepository.findStockInfoBySearchKeyword(keyword);
+
+        return stockInfoList.stream()
+                .map(stockInfo -> new StockAutocompleteInfoDto(
+                        stockInfo.getSymbol(),
+                        stockInfo.getName()
+                    )
                 ).collect(Collectors.toList());
-
     }
 
-    public Long addStarStock(AddStarRequest request){
-        User user = userRepository.findUserById(request.getUserId());
+    /**
+     * Fetches the latest stock data from Alpha Vantage API and saves it to the database.
+     *
+     * @param symbol Stock symbol
+     * @return       saved Stock object
+     */
+    private Stock getUpdatedStockData(String symbol) {
 
-        if(user == null)
-            throw new RuntimeException("User not found");
+        log.info("Getting stock data for symbol: {} from Alpha Vantage API", symbol);
 
-        StarStock starStock = new StarStock()
-                .setUser(user)
-                .setStock(request.getStock())
-                .setPrice(request.getPrice());
-        StarStock saved = starStockRepository.save(starStock);
-        return saved.getId();
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .queryParam("function", "GLOBAL_QUOTE")
+                .queryParam("symbol", symbol)
+                .queryParam("apikey", apiKey);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<AVGlobalQuoteResponse> response = restTemplate
+                .exchange(uriBuilder.toUriString(), HttpMethod.GET, null, AVGlobalQuoteResponse.class);
+
+        if (response.getStatusCode().isError()) {
+            log.error("Could not get stock data for symbol: {}", symbol);
+            throw new ApiRequestException("Could not get stock data for symbol: " + symbol, response.getStatusCode().value());
+        }
+
+        if (response.getBody() == null) {
+            log.error("Stock data is null for symbol: {}", symbol);
+            throw new ApiRequestException("Could not get stock data for symbol: " + symbol, response.getStatusCode().value());
+        }
+
+        AVGlobalQuoteResponse.GlobalQuote avGlobalQuoteResponse = response.getBody().getGlobalQuote();
+        return new Stock(
+                null,
+                avGlobalQuoteResponse.getSymbol(),
+                parseLatestTradingDay(avGlobalQuoteResponse.getLatestTradingDay()),
+                Double.parseDouble(avGlobalQuoteResponse.getOpen()),
+                Double.parseDouble(avGlobalQuoteResponse.getHigh()),
+                Double.parseDouble(avGlobalQuoteResponse.getLow()),
+                Double.parseDouble(avGlobalQuoteResponse.getPreviousClose()),
+                Double.parseDouble(avGlobalQuoteResponse.getPrice()),
+                Long.parseLong(avGlobalQuoteResponse.getVolume())
+        );
+    }
+
+    private Timestamp parseLatestTradingDay(String latestTradingDay) {
+        return Timestamp.valueOf(latestTradingDay + " 00:00:00");
+    }
+
+    private Boolean isBeforeToday(Timestamp date) {
+        return date.toLocalDateTime().toLocalDate().isBefore(LocalDate.now());
     }
 
 }
