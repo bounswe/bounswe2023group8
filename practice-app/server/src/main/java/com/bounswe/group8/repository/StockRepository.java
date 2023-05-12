1b160ab6a127b9871891c81d96d354a8858abc11
bahadirgezer <bahadirgezerbg@gmail.com>
2023-05-12 20:42:34 +0300

    add projection for stocks and repository methods
    stock related

--- practice-app/server/src/main/java/com/bounswe/group8/repository/StockRepository.java
+++ practice-app/server/src/main/java/com/bounswe/group8/repository/StockRepository.java
@@ -3,8 +3,12 @@ package com.bounswe.group8.repository;
 import com.bounswe.group8.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     Stock findFirstStockBySymbolOrderByTradingDayDesc(String symbol);
+
+    Set<Stock> findAllBySymbolContainingIgnoreCase(String symbol);
 
 }
