86f6a43dea72535bfb3343b2ced05c4c83a66ed9
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-17 15:47:29 +0300

    add init.sh to intialize mysql/init.sql file

--- app/backend/enigma/init.sh
+++ app/backend/enigma/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" > init.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
+# shellcheck disable=SC2046
+export $(xargs <.env)
+
+# initialize mysql/init.sql
+mkdir mysql
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" > mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> mysql/init.sql
+echo "FLUSH PRIVILEGES;" >> mysql/init.sql
