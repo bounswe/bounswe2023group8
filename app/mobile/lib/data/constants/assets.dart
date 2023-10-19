8a07a76d4dc89c48116b74d46a1af6c3240161fd
celikomerfaruk <01omerfarukcelik@gmail.com>
2023-10-19 14:26:43 +0300

    static data added

--- app/mobile/lib/data/constants/assets.dart
+++ app/mobile/lib/data/constants/assets.dart
@@ -1,3 +1,5 @@
 class Assets {
-  static const bellIcon = 'xyz.svg';
+  static const String logo = 'assets/icons/logo.png';
+  static const String eye = 'assets/icons/eye.svg';
+  static const String eyeClosed = 'assets/icons/eye-closed.svg';
 }
