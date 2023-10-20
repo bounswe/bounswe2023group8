a8afe4c8f28c85d77e59a254f7ec99cb16ed3c2b
Bahri Alabey <alabeybb@gmail.com>
2023-10-20 14:25:43 +0300

    rename containers as pages

--- app/web/src/index.jsx
+++ app/web/src/index.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./pages/App/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
