d226065c08c09518be27911b3f02170fdbc35805
begumyivli <begumm0532@icloud.com>
2023-10-21 15:04:20 +0300

    added logo and continue as visitor and arranged page

--- app/mobile/lib/modules/opening/views/opening_view.dart
+++ app/mobile/lib/modules/opening/views/opening_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:mobile/data/widgets/custom_button.dart';
 import 'package:mobile/modules/opening/controllers/opening_controller.dart';
+import 'package:mobile/data/constants/assets.dart';
+import 'package:mobile/data/constants/palette.dart';
 
 class OpeningView extends GetView<OpeningController> {
   const OpeningView({super.key});
@@ -10,24 +11,58 @@ class OpeningView extends GetView<OpeningController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox(
-      width: Get.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomButton(
-              text: 'Log in',
-              onPressed: () {
-                controller.navigateToAuthentication(toLogin: true);
-              }),
-          const SizedBox(height: 16),
-          CustomButton(
-              text: 'Sign up',
-              onPressed: () {
-                controller.navigateToAuthentication(toLogin: false);
-              }),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 100), // Add space above the content
+            Image.asset(
+              Assets.logo,
+              height: Get.height * 0.2,
+              fit: BoxFit.contain,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(height: 40), // Add space between login and logo
+                  CustomButton(
+                    text: 'Log in',
+                    onPressed: () {
+                      controller.navigateToAuthentication(toLogin: true);
+                    },
+                  ),
+                  SizedBox(height: 10), // Add space between "Log in" and "Sign Up"
+                  CustomButton(
+                    text: 'Sign Up',
+                    onPressed: () {
+                      controller.navigateToAuthentication(toLogin: false);
+                    },
+                  ),
+                  SizedBox(height: 10), // Add space between "Sign Up" and "or"
+                  Text(
+                    'or',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10), // Add space between "or" and "Continue as Visitor"
+                  InkWell(
+                    onTap: () {
+                      // Handle "Continue as Visitor" click
+                    },
+                    child: Text(
+                      'Continue as Visitor',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Palette.primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
