30992daeca5beca0b4c6e1b879a777b874b02a70
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-10-23 21:05:52 +0300

    implement generateToken function to allow Jwt token generation

--- app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtService.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtService.java
@@ -1,10 +1,13 @@
 package com.wia.enigma.core.service;
 
 import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface EnigmaJwtService {
 
     void validateJwt(String jwt);
 
     Claims extractClaims(String jwt);
+
+    String generateToken(UserDetails userDetails);
 }
--- app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtServiceImpl.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtServiceImpl.java
@@ -2,11 +2,20 @@ package com.wia.enigma.core.service;
 
 import com.wia.enigma.dal.repository.EnigmaJwtRepository;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Slf4j
 @Service
@@ -14,7 +23,12 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EnigmaJwtServiceImpl implements EnigmaJwtService {
 
-    final EnigmaJwtRepository jwtRepository;
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
 
     /**
      * Validates the JWT.
@@ -36,9 +50,25 @@ public class EnigmaJwtServiceImpl implements EnigmaJwtService {
      */
     @Override
     public Claims extractClaims(String jwt) {
-
         return null;
     }
+
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", userDetails.getUsername());
+        claims.put("roles", userDetails.getAuthorities());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
 
 
 }
