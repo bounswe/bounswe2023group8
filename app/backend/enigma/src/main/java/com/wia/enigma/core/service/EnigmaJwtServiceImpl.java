074a84442268cdbf21f8e6f2ef3e0fb359762bc6
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-25 01:03:24 +0300

    fill in enigma jwt service methods

--- app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtServiceImpl.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtServiceImpl.java
@@ -1,20 +1,28 @@
 package com.wia.enigma.core.service;
 
+import com.wia.enigma.configuration.security.EnigmaAuthenticationToken;
+import com.wia.enigma.core.data.dto.JwtGenerationDto;
+import com.wia.enigma.dal.entity.EnigmaJwt;
+import com.wia.enigma.dal.enums.AudienceType;
+import com.wia.enigma.dal.enums.ExceptionCodes;
 import com.wia.enigma.dal.repository.EnigmaJwtRepository;
+import com.wia.enigma.exceptions.custom.EnigmaBadRequestException;
+import com.wia.enigma.exceptions.custom.EnigmaDatabaseException;
+import com.wia.enigma.exceptions.custom.EnigmaUnauthorizedException;
+import com.wia.enigma.utilities.AuthUtils;
+import com.wia.enigma.utilities.JwtUtils;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
 
 
 @Slf4j
@@ -23,52 +31,171 @@ import java.util.Map;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EnigmaJwtServiceImpl implements EnigmaJwtService {
 
+    final EnigmaJwtRepository enigmaJwtRepository;
 
-    @Value("${jwt.secret}")
-    private String SECRET_KEY;
+    @Value("${jwt.expiration.access-token}")
+    Long accessTokenExpiration;
 
-    @Value("${jwt.expiration}")
-    private long jwtExpiration;
+    @Value("${jwt.expiration.refresh-token}")
+    Long refreshTokenExpiration;
 
     /**
      * Validates the JWT.
-     * If the JWT is invalid, throws an exception.
      *
      * @param jwt JWT String
+     * @return EnigmaAuthenticationToken with the claims of the JWT
      */
     @Override
-    public void validateJwt(String jwt) {
+    public EnigmaAuthenticationToken validateJwt(String jwt) {
 
+        Claims claims = JwtUtils.getInstance().extractClaims(jwt);
+        if (claims == null)
+            throw new EnigmaUnauthorizedException(ExceptionCodes.INVALID_JWT,
+                    "Could not extract claims from JWT.");
+
+        long jti;
+        try {
+            jti = Long.parseLong(claims.getId());
+        } catch (Exception e) {
+            throw new EnigmaUnauthorizedException(ExceptionCodes.INVALID_JWT_ID,
+                    "Invalid JWT id.");
+        }
+
+        EnigmaJwt enigmaJwt;
+        try {
+            enigmaJwt = enigmaJwtRepository.findEnigmaJwtById(jti);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new EnigmaDatabaseException(ExceptionCodes.DB_GET_ERROR,
+                    "Could not get EnigmaJwt.");
+        }
+
+        if (enigmaJwt == null)
+            throw new EnigmaUnauthorizedException(ExceptionCodes.INVALID_JWT_ID,
+                    "EnigmaJwt not found for id: " + claims.getId());
+
+        if (enigmaJwt.getRevokedAt() != null)
+            throw new EnigmaUnauthorizedException(ExceptionCodes.REVOKED_JWT,
+                    "EnigmaJwt is revoked.");
+
+        if (enigmaJwt.getExpiresAt().before(new Timestamp(System.currentTimeMillis())))
+            throw new EnigmaUnauthorizedException(ExceptionCodes.INVALID_JWT_EXPIRATION,
+                    "EnigmaJwt is expired.");
+
+        return new EnigmaAuthenticationToken(
+                claims.getSubject(),
+                null,
+                ((Integer) claims.get("user_id")).longValue(),
+                AudienceType.fromAudienceSet(claims.getAudience()),
+                Long.parseLong(claims.getId()),
+                AuthUtils.getInstance().buildAuthorities(claims.get("authorities"))
+        );
     }
 
     /**
-     * Extract all claims from the JWT.
-     * Claims are the payload (body) of the JWT.
+     * Revokes the token.
      *
-     * @param jwt JWT String
-     * @return Claims
+     * @param enigmaJwtId  EnigmaJwt.Id
      */
     @Override
-    public Claims extractClaims(String jwt) {
-        return null;
+    public void revokeToken(Long enigmaJwtId) {
+
+        EnigmaJwt enigmaJwt;
+        try {
+            enigmaJwt = enigmaJwtRepository.findEnigmaJwtById(enigmaJwtId);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new EnigmaDatabaseException(ExceptionCodes.DB_GET_ERROR,
+                    "Could not get EnigmaJwt.");
+        }
+
+        if (enigmaJwt == null)
+            throw new EnigmaBadRequestException(ExceptionCodes.INVALID_JWT_ID,
+                    "EnigmaJwt not found for id: " + enigmaJwtId);
+
+        enigmaJwt.setRevokedAt(new Timestamp(System.currentTimeMillis()));
+        try {
+            enigmaJwtRepository.save(enigmaJwt);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new EnigmaDatabaseException(ExceptionCodes.DB_SAVE_ERROR,
+                    "Could not save EnigmaJwt.");
+        }
     }
 
-
+    /**
+     * Generates a new access token or refresh token.
+     *
+     * @param enigmaUserId      EnigmaUser.Id
+     * @param username          EnigmaUser.username
+     * @param authorities       Authorities List of EnigmaUser
+     * @param audienceValue     EnigmaUser.audienceValue
+     * @param isRefreshToken    specifies if the token is a refresh token
+     * @return                  JwtGenerationDto
+     */
     @Override
-    public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("sub", userDetails.getUsername());
-        claims.put("roles", userDetails.getAuthorities());
+    public JwtGenerationDto generateToken(Long enigmaUserId,
+                                          String username,
+                                          List<String> authorities,
+                                          String audienceValue,
+                                          Boolean isRefreshToken) {
 
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
+        return generateToken(enigmaUserId, username, authorities, audienceValue, new Timestamp(System.currentTimeMillis()), isRefreshToken);
     }
 
+    /**
+     * Generates a new access token and refresh token.
+     *
+     * @param enigmaUserId      EnigmaUser.Id
+     * @param username          EnigmaUser.username
+     * @param authorities       Authorities List of EnigmaUser
+     * @param audienceValue     EnigmaUser.audienceValue
+     * @return                  Pair: first -> access token, second -> refresh token
+     */
+    @Override
+    public Pair<JwtGenerationDto, JwtGenerationDto> generateTokens(Long enigmaUserId,
+                                                                   String username,
+                                                                   List<String> authorities,
+                                                                   String audienceValue) {
 
+        Timestamp now = new Timestamp(System.currentTimeMillis());
 
+        JwtGenerationDto accessToken = generateToken(enigmaUserId, username, authorities, audienceValue, now, false);
+        JwtGenerationDto refreshToken = generateToken(enigmaUserId, username, authorities, audienceValue, now, true);
+
+        return Pair.of(accessToken, refreshToken);
+    }
+
+    private JwtGenerationDto generateToken(Long enigmaUserId,
+                                           String username,
+                                           List<String> authorities,
+                                           String audienceValue,
+                                           Timestamp issuedAt,
+                                           Boolean isRefreshToken) {
+
+        Long jti = UUID.randomUUID().getLeastSignificantBits();
+        Timestamp expiresAt = new Timestamp(issuedAt.getTime() +
+                (isRefreshToken ? refreshTokenExpiration : accessTokenExpiration));
+
+        EnigmaJwt enigmaJwt = EnigmaJwt.builder()
+                .id(jti)
+                .enigmaUserId(enigmaUserId)
+                .issuedAt(issuedAt)
+                .expiresAt(expiresAt)
+                .isRefreshToken(isRefreshToken)
+                .build();
+
+        try {
+            enigmaJwtRepository.save(enigmaJwt);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new EnigmaDatabaseException(ExceptionCodes.DB_SAVE_ERROR,
+                    "Cannot save Enigma Jwt.");
+        }
+
+        String jwt = JwtUtils.getInstance()
+                .generateToken(enigmaUserId, username, authorities, jti, audienceValue, issuedAt, expiresAt);
+
+        return new JwtGenerationDto(jwt, issuedAt, (isRefreshToken ? refreshTokenExpiration : accessTokenExpiration));
+    }
 }
