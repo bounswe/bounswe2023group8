c7e9bd0c8f40d38623b793f0d78c892a583c63de
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-25 00:55:09 +0300

    add ExceptionCodes enum, modify AudienceType enum
    for token generation

--- app/backend/enigma/src/main/java/com/wia/enigma/dal/enums/AudienceType.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/dal/enums/AudienceType.java
@@ -1,5 +1,7 @@
 package com.wia.enigma.dal.enums;
 
+import java.util.Set;
+
 public enum AudienceType {
 
     USER("user"),
@@ -13,5 +15,38 @@ public enum AudienceType {
 
     public String getName() {
         return name;
+    }
+
+    public static AudienceType fromString(String text) {
+        for (AudienceType b : AudienceType.values()) {
+            if (b.name.equalsIgnoreCase(text)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public static AudienceType fromAudienceSet(Set<String> audienceSet) {
+
+        if (audienceSet == null || audienceSet.size() != 1)
+            return null;
+
+        for (AudienceType b : AudienceType.values()) {
+            if (audienceSet.contains(b.name)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public static String getTypes() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (AudienceType b : AudienceType.values()) {
+            sb.append(b.name).append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        sb.append("]");
+        return sb.toString();
     }
 }
