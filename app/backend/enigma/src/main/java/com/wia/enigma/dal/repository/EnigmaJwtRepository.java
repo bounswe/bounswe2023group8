eeb0c86fd56eb8a011ff4f4cab36edbe70710e42
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-25 00:56:16 +0300

    add repository layer query methods for enigma jwt and user models

--- app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/EnigmaJwtRepository.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/EnigmaJwtRepository.java
@@ -2,7 +2,9 @@ package com.wia.enigma.dal.repository;
 
 import com.wia.enigma.dal.entity.EnigmaJwt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EnigmaJwtRepository extends JpaRepository<EnigmaJwt, Long> {
 
+    EnigmaJwt findEnigmaJwtById(Long id);
 }
--- app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/EnigmaUserRepository.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/EnigmaUserRepository.java
@@ -1,10 +1,40 @@
 package com.wia.enigma.dal.repository;
 
 import com.wia.enigma.dal.entity.EnigmaUser;
+import com.wia.enigma.dal.projection.EnigmaUserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EnigmaUserRepository extends JpaRepository<EnigmaUser, Long> {
 
-    EnigmaUser findByUsername(String username);
+    /*
+        TODO: Implement this method
 
+         username,
+         password,
+         id,
+         audienceType,
+         authorities (List<String>)
+     */
+    @Query("SELECT u " +
+            "FROM EnigmaUser u " +
+            "WHERE u.username = ?1 AND u.isDeleted = false")
+    EnigmaUserDetailsProjection findUserDetailsByUsername(String username);
+
+    @Query("SELECT CASE WHEN COUNT(e) > 0 THEN true ELSE false END " +
+            "FROM EnigmaUser e " +
+            "WHERE ((:username IS NULL OR e.username = :username) " +
+            "OR (:email IS NULL OR e.email = :email)) " +
+            "AND e.isDeleted = false")
+    boolean existsByUsernameOrEmail(String username, String email);
+
+    @Query("SELECT u " +
+            "FROM EnigmaUser u " +
+            "WHERE u.username = ?1 AND u.isDeleted = false")
+    EnigmaUser findEnigmaUserByUsername(String username);
+
+    @Query("SELECT u " +
+            "FROM EnigmaUser u " +
+            "WHERE u.email = ?1 AND u.isDeleted = false")
+    EnigmaUser findEnigmaUserByEmail(String email);
 }
