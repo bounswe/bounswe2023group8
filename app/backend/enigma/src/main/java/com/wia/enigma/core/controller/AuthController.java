2a06ec9f953685be6298d4ea969704109d94a877
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-25 01:03:44 +0300

    create login, register, logout endpoint in the
    AuthController

--- app/backend/enigma/src/main/java/com/wia/enigma/core/controller/AuthController.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/controller/AuthController.java
@@ -1,19 +1,31 @@
 package com.wia.enigma.core.controller;
 
 
+import com.wia.enigma.configuration.security.EnigmaAuthenticationToken;
 import com.wia.enigma.configuration.security.EnigmaUserDetailsService;
+import com.wia.enigma.core.data.request.SignupRequest;
+import com.wia.enigma.core.data.response.LoginResponse;
+import com.wia.enigma.core.data.response.RegisterResponse;
 import com.wia.enigma.core.service.EnigmaJwtService;
+import com.wia.enigma.core.service.EnigmaUserService;
 import com.wia.enigma.dal.entity.EnigmaUser;
 import com.wia.enigma.dal.repository.EnigmaUserRepository;
+import com.wia.enigma.exceptions.custom.EnigmaUnauthorizedException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.apache.coyote.Response;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,42 +33,66 @@ import org.springframework.web.bind.annotation.*;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthController {
 
-    final EnigmaUserRepository userRepository;
-    final EnigmaUserDetailsService userDetailsService;
-    final EnigmaJwtService jwtService;
+    final EnigmaUserService enigmaUserService;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestParam String username,
-                                   @RequestParam String password) {
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if (userDetails != null && new BCryptPasswordEncoder().matches(password, userDetails.getPassword())) {
-            String token = jwtService.generateToken(userDetails);
-            return ResponseEntity.ok().header("Authorization", "Bearer " + token).build();
-        }
+    /**
+     * WA-1: Signs up a new user.
+     */
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequest signupRequest) {
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        RegisterResponse registerResponse = enigmaUserService.registerEnigmaUser(
+                signupRequest.getUsername(),
+                signupRequest.getEmail(),
+                signupRequest.getPassword(),
+                signupRequest.getBirthday()
+        );
+
+        if (registerResponse == null)
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+
+        return ResponseEntity
+                .created(
+                        ServletUriComponentsBuilder
+                                .fromCurrentContextPath()
+                                .path("/api/v1/user/{enigmaUserId}")
+                                .buildAndExpand(registerResponse.getEnigmaUserId())
+                                .toUri()
+                )
+                .body(registerResponse);
     }
 
+    /**
+     * WA-2: Signs in a user.
+     */
+    @GetMapping("/signin")
+    public ResponseEntity<?> signin(@Valid @NotNull @RequestParam(name = "user") String usernameOrEmail,
+                                    @Valid @NotNull @RequestParam String password) {
 
-    @GetMapping("/refresh")
-    public ResponseEntity<?> refresh(@RequestParam String refreshToken) {
+        LoginResponse loginResponse = enigmaUserService.loginEnigmaUser(usernameOrEmail, password);
 
-        return ResponseEntity.ok().build();
+        if (loginResponse == null)
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(loginResponse);
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestParam String username,
-                                      @RequestParam String email,
-                                      @RequestParam String password) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    /**
+     * WA-3: Signs out a user.
+     */
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
 
-        EnigmaUser newUser = new EnigmaUser();
-        newUser.setUsername(username);
-        newUser.setEmail(email);
-        newUser.setPassword(encoder.encode(password));
+        enigmaUserService.logoutEnigmaUser(request.getHeader(HttpHeaders.AUTHORIZATION));
 
-        userRepository.save(newUser);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
     }
 }
