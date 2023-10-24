0457aee681634a10be37326b7e4f663e89c50cce
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-25 00:59:26 +0300

    add user and jwt service interfaces

--- app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtService.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/service/EnigmaJwtService.java
@@ -1,13 +1,28 @@
 package com.wia.enigma.core.service;
 
+import com.wia.enigma.configuration.security.EnigmaAuthenticationToken;
+import com.wia.enigma.configuration.security.EnigmaUserDetails;
+import com.wia.enigma.core.data.dto.JwtGenerationDto;
 import io.jsonwebtoken.Claims;
+import org.springframework.data.util.Pair;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
 
 public interface EnigmaJwtService {
 
-    void validateJwt(String jwt);
+    EnigmaAuthenticationToken validateJwt(String jwt);
 
-    Claims extractClaims(String jwt);
+    JwtGenerationDto generateToken(Long enigmaUserId,
+                                   String username,
+                                   List<String> authorities,
+                                   String audienceValue,
+                                   Boolean isRefreshToken);
 
-    String generateToken(UserDetails userDetails);
+    Pair<JwtGenerationDto, JwtGenerationDto> generateTokens(Long enigmaUserId,
+                                                            String username,
+                                                            List<String> authorities,
+                                                            String audienceValue);
+
+    void revokeToken(Long enigmaJwtId);
 }
