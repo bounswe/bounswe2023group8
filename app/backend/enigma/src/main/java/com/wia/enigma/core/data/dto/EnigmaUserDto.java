88d594cb4ef5ce0c18edd560d76fb552d1b8bdcf
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-25 00:59:00 +0300

    add enigma user dto

---
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/data/dto/EnigmaUserDto.java
@@ -0,0 +1,29 @@
+package com.wia.enigma.core.data.dto;
+
+import com.wia.enigma.dal.entity.EnigmaUser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class EnigmaUserDto {
+
+    Long id;
+
+    String username;
+
+    String name;
+
+    String email;
+
+    Date birthday;
+
+    Timestamp createdTime;
+}
