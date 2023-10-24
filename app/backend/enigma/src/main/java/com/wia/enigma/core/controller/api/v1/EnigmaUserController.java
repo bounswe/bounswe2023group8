8e5c03b14f0ddd1bac45b65e17ee8257c593c55b
bahadirgezer <bahadirgezerbg@gmail.com>
2023-10-25 01:00:52 +0300

    add new endpoint for test purposes

--- app/backend/enigma/src/main/java/com/wia/enigma/core/controller/api/v1/EnigmaUserController.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/controller/api/v1/EnigmaUserController.java
@@ -1,16 +1,29 @@
 package com.wia.enigma.core.controller.api.v1;
 
+import com.wia.enigma.configuration.security.EnigmaAuthenticationToken;
 import com.wia.enigma.core.service.EnigmaUserServiceImpl;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EnigmaUserController {
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test(EnigmaAuthenticationToken token) {
+        log.info("token: {}", token);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
