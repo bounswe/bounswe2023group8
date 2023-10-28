309f65d9c55a3b333a393a040e6828d2c4a2e410
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-10-29 00:16:01 +0300

    implement verification response and repository

---
+++ app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/VerificationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.wia.enigma.dal.repository;
+
+import com.wia.enigma.core.data.response.VerificationResponse;
+import com.wia.enigma.dal.entity.Post;
+import com.wia.enigma.dal.entity.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+
+    VerificationToken findByTokenAndIsResetPasswordToken(String token, Boolean isResetPasswordToken);
+}
