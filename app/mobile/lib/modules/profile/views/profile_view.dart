db787e848872f70fe7e9222f6c9b42ded342383c
begumyivli <begumm0532@icloud.com>
2023-10-28 19:43:07 +0300

    added following followers pop up buttons

--- app/mobile/lib/modules/profile/views/profile_view.dart
+++ app/mobile/lib/modules/profile/views/profile_view.dart
@@ -4,8 +4,9 @@ import 'package:get/get.dart';
 
 import '../controllers/profile_controller.dart';
 
+
 class ProfileView extends GetView<ProfileController> {
-  const ProfileView({super.key});
+  const ProfileView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +15,125 @@ class ProfileView extends GetView<ProfileController> {
         title: const Text('ProfileView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Column(
+          children: [
+            ProfileHeader(
+              followersCount: 20,
+              followingCount: 25,
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class ProfileHeader extends StatelessWidget {
+  final int followersCount;
+  final int followingCount;
+
+  const ProfileHeader({
+    Key? key,
+    required this.followersCount,
+    required this.followingCount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+         Padding( // wrapped with padding to add some distance between 2 text
+          padding: const EdgeInsets.only(right: 10), 
+            child: InkWell(
+            onTap: () {
+              Get.dialog(FollowersPopup());
+            },
+            child: Text('$followersCount followers'),
+          )
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+            child: InkWell(
+            onTap: () {
+              Get.dialog(FollowingsPopup());
+            },
+            child: Text('$followingCount followings'),
+          )
+        ),
+      ],
+    );
+  }
+}
+
+class FollowersPopup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        dialogBackgroundColor: Color(0xFFF1F1F1), // Set the background color
+      ),
+      child: SimpleDialog(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Followers'),
+            IconButton(
+              icon: Icon(Icons.close), // Close icon
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        ),
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              // Follower's photo
+              backgroundImage: AssetImage('assets/icons/begumpp.jpeg'),
+            ),
+            title: Text('Begüm Yivli'),
+            subtitle: Text('@begum'),
+          ),
+          // Add more ListTile widgets for additional followers
+        ],
+      ),
+    );
+  }
+}
+
+class FollowingsPopup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        dialogBackgroundColor: Color(0xFFF1F1F1), // Set the background color
+      ),
+    child: SimpleDialog(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Followers'),
+          IconButton(
+            icon: Icon(Icons.close), // Close icon
+            onPressed: () {
+              Navigator.of(context).pop(); // Close the dialog
+            },
+          ),
+        ],
+      ),
+      children: [
+        ListTile(
+          leading: CircleAvatar(
+            // Following's photo
+            backgroundImage: AssetImage('assets/icons/begumpp.jpeg'),
+          ),
+          title: Text('Begüm Yivli'),
+          subtitle: Text('@begum'),
+        ),
+        // Add more ListTile widgets for additional followers
+      ],
+    ),
     );
   }
 }
