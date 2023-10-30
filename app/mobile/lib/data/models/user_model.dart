0e5596c9fb0470fd888ed599e49de0523c1d344e
celikomerfaruk <01omerfarukcelik@gmail.com>
2023-10-31 01:00:54 +0300

    profile view finished

--- app/mobile/lib/data/models/user_model.dart
+++ app/mobile/lib/data/models/user_model.dart
@@ -1,15 +1,37 @@
-class User {
+class UserModel {
+  final int id;
   final String name;
   final String username;
-  final String profileImage;
-  final int likeCount;
-  final int dislikeCount;
+  final List<int> iaIds;
+  final int followerCount;
+  final int followingCount;
+  final int allTimeLikes;
+  final int allTimeDislikes;
+  final String userProfileImage;
 
-  User({
+  const UserModel({
+    required this.id,
     required this.name,
     required this.username,
-    required this.profileImage,
-    required this.likeCount,
-    required this.dislikeCount,
+    required this.iaIds,
+    required this.followerCount,
+    required this.followingCount,
+    required this.allTimeLikes,
+    required this.allTimeDislikes,
+    required this.userProfileImage,
   });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      name: json['name'],
+      username: json['nickname'],
+      iaIds: json['ia_ids'].cast<int>(),
+      followerCount: json['follower_count'],
+      followingCount: json['following_count'],
+      allTimeLikes: json['all_time_likes'],
+      allTimeDislikes: json['all_time_dislikes'],
+      userProfileImage: json['user_profile_image'],
+    );
+  }
 }
