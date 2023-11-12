418856c8dda95032f260fb8cf0f4e98b6c1364bb
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-11-12 19:13:00 +0300

    create EntityTagsRepository

---
+++ app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/EntityTagsRepository.java
@@ -0,0 +1,9 @@
+package com.wia.enigma.dal.repository;
+
+import com.wia.enigma.dal.entity.EntityTags;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntityTagsRepository extends JpaRepository<EntityTags, Long> {
+
+
+}
