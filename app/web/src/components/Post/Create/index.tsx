edbee6233c7c6d95bc40adbc59f19dcabc9c375c
Bahri Alabey <alabeybb@gmail.com>
2023-11-13 19:39:35 +0300

    fix styling

--- app/web/src/components/Post/Create/index.tsx
+++ app/web/src/components/Post/Create/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useCreatePost from "../../../hooks/usePost";
 import { useAuth } from "../../../contexts/AuthContext";
+import Tag from "../../Tag/Tag";
 
 export type CreatePostFormData = {
   interestArea: string;
@@ -29,12 +30,20 @@ const CreatePost = () => {
   };
 
   const [postDetails, setPostDetails] = useState(defaultPostDetails);
+  const [locationModalOpen, setLocationModalOpen] = useState(false);
 
-  const handleInputChange = (e: any) => {
-    const { name, value } = e.target;
+  const handleInputChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target as typeof e.target & {
+      value: string;
+      name: string;
+    };
     if (name === "tags") {
-      // Assuming tags are space separated
-      const tagsArray = value.split(" ").filter((tag: string) => tag !== ""); // Filter out any empty strings
+      const tagsArray = value.split(" ").filter((tag: string) => tag !== "");
       setPostDetails({
         ...postDetails,
         [name]: tagsArray,
@@ -45,7 +54,6 @@ const CreatePost = () => {
         [name]: new Date(value),
       });
     } else if (name === "location") {
-      // Assuming location is a single string, you'll need to adjust this if it's not
       setPostDetails({
         ...postDetails,
         [name]: [value],
@@ -58,10 +66,22 @@ const CreatePost = () => {
     }
   };
 
+  const [newTag, setNewTag] = useState("");
+
+  const addTag = () => {
+    if (newTag && !postDetails.tags.includes(newTag)) {
+      setPostDetails({
+        ...postDetails,
+        tags: [...postDetails.tags, newTag],
+      });
+      setNewTag("");
+    }
+  };
+
   const { mutate } = useCreatePost({});
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
     mutate({
       axiosInstance,
       ...postDetails,
@@ -69,153 +89,163 @@ const CreatePost = () => {
   };
 
   return (
-    <div className="container mt-4">
-      <h2>Create a New Post!</h2>
-      <form onSubmit={handleSubmit}>
-        {/* Interest Area */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="interestArea" className="form-label me-2">
-            Interest Area:
-          </label>
-          <select
-            id="interestArea"
-            className="form-select"
-            name="interestArea"
-            value={postDetails.interestArea}
-            onChange={handleInputChange}
-          >
-            <option value="Furkanin Futbol Köşesi">
-              Furkanin Futbol Köşesi
-            </option>
-            {/* Add other options here */}
-          </select>
-        </div>
+    <div className="d-flex">
+      <div className="container mt-4 col-6">
+        <h2 className="fw-bold">Create a New Post!</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3 d-flex flex-row align-items-center">
+            <label htmlFor="interestArea" className="form-label text-nowrap">
+              Interest Area:
+            </label>
+            <select
+              id="interestArea"
+              className="form-select bg-dark-subtle ms-5"
+              name="interestArea"
+              value={postDetails.interestArea}
+              onChange={handleInputChange}
+            >
+              <option value="Furkanin Futbol Köşesi">
+                Furkanın Futbol Köşesi
+              </option>
+            </select>
+          </div>
+          <div className="mb-3 d-flex flex-row align-items-center">
+            <label htmlFor="title" className="form-label text-nowrap">
+              Title:
+            </label>
+            <input
+              id="title"
+              type="text"
+              className="form-control bg-dark-subtle ms-5"
+              name="title"
+              value={postDetails.title}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        {/* Title */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="title" className="form-label me-2">
-            Title:
-          </label>
-          <input
-            id="title"
-            type="text"
-            className="form-control"
-            name="title"
-            value={postDetails.title}
-            onChange={handleInputChange}
-          />
-        </div>
+          {/* Link */}
+          <div className="mb-3 d-flex flex-row align-items-center">
+            <label htmlFor="link" className="form-label text-nowrap">
+              Link:
+            </label>
+            <input
+              id="link"
+              type="text"
+              className="form-control bg-dark-subtle ms-5"
+              name="link"
+              value={postDetails.link}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-3 d-flex flex-row align-items-start">
+            <label htmlFor="description" className="form-label text-nowrap">
+              Description/Comment:
+            </label>
+            <textarea
+              id="description"
+              className="form-control bg-dark-subtle ms-5"
+              name="description"
+              value={postDetails.description}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="tags" className="form-label text-nowrap">
+              Tags:
+            </label>
+            <div
+              className="d-flex flex-row align-items-center"
+              style={{ gap: "0.5rem" }}
+            >
+              <div
+                className="d-flex flex-wrap flex-grow-1"
+                style={{ gap: "0.5rem" }}
+              >
+                {postDetails.tags.map((tag, index) => (
+                  <div key={index} className="m-2">
+                    <Tag className={""} name={tag} />
+                  </div>
+                ))}
+              </div>
+              <div className="d-flex align-items-center">
+                {" "}
+                {/* This div for input and button should stay on the right */}
+                <input
+                  type="text"
+                  className="form-control bg-dark-subtle ms-5"
+                  value={newTag}
+                  onChange={(e) => setNewTag(e.target.value)}
+                  onKeyPress={(e) => e.key === "Enter" && addTag()}
+                  style={{ width: "150px" }} // Fixed width for input
+                />
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={addTag}
+                  style={{ marginLeft: "0.5rem" }} // Add margin to separate from input
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="mb-3 d-flex flex-row align-items-center">
+            <label htmlFor="label" className="form-label text-nowrap">
+              Label:
+            </label>
+            <select
+              id="label"
+              className="form-select bg-dark-subtle ms-5"
+              name="label"
+              value={postDetails.label}
+              onChange={handleInputChange}
+            >
+              <option value="News">News</option>
+            </select>
+          </div>
+          <div className="mb-3 d-flex flex-row align-items-center">
+            <label htmlFor="source" className="form-label text-nowrap">
+              Source:
+            </label>
+            <input
+              id="source"
+              type="text"
+              className="form-control bg-dark-subtle ms-5"
+              name="source"
+              value={postDetails.source}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-3 d-flex flex-row align-items-center">
+            <label htmlFor="publicationDate" className="form-label text-nowrap">
+              Publication Date:
+            </label>
+            <input
+              id="publicationDate"
+              type="date"
+              className="form-control bg-dark-subtle ms-5"
+              name="publicationDate"
+              value={postDetails.publicationDate.toISOString().substring(0, 10)}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-3 d-flex flex-row align-items-center">
+            <button
+              className="btn btn-secondary"
+              onClick={() => setLocationModalOpen(true)}
+            >
+              Add Location
+            </button>
+          </div>
 
-        {/* Link */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="link" className="form-label me-2">
-            Link:
-          </label>
-          <input
-            id="link"
-            type="url"
-            className="form-control"
-            name="link"
-            value={postDetails.link}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        {/* Description/Comment */}
-        <div className="mb-3 d-flex flex-row align-items-start">
-          <label htmlFor="description" className="form-label me-2">
-            Description/Comment:
-          </label>
-          <textarea
-            id="description"
-            className="form-control"
-            name="description"
-            value={postDetails.description}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        {/* Tags */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="tags" className="form-label me-2">
-            Tags:
-          </label>
-          <input
-            id="tags"
-            type="text"
-            className="form-control"
-            name="tags"
-            value={postDetails.tags.join(" ")}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        {/* Label */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="label" className="form-label me-2">
-            Label:
-          </label>
-          <select
-            id="label"
-            className="form-select"
-            name="label"
-            value={postDetails.label}
-            onChange={handleInputChange}
-          >
-            <option value="News">News</option>
-            {/* Add other options here */}
-          </select>
-        </div>
-
-        {/* Source */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="source" className="form-label me-2">
-            Source:
-          </label>
-          <input
-            id="source"
-            type="text"
-            className="form-control"
-            name="source"
-            value={postDetails.source}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        {/* Publication Date */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="publicationDate" className="form-label me-2">
-            Publication Date:
-          </label>
-          <input
-            id="publicationDate"
-            type="date"
-            className="form-control"
-            name="publicationDate"
-            value={postDetails.publicationDate.toISOString().substring(0, 10)}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        {/* Location */}
-        <div className="mb-3 d-flex flex-row align-items-center">
-          <label htmlFor="location" className="form-label me-2">
-            Location:
-          </label>
-          <input
-            id="location"
-            type="text"
-            className="form-control"
-            name="location"
-            value={postDetails.location.join(", ")}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <button type="submit" className="btn btn-primary">
-          Create Post
-        </button>
-      </form>
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-primary">
+              Create Post
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="col-6 mt-5"></div>
     </div>
   );
 };
