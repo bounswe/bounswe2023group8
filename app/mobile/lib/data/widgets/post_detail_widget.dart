3c2f4adf4ee0cc984836310e62a09509be5583d8
Meriç Keskin <meric.keskin@external.yapikredi.com.tr>
2023-11-14 12:01:38 +0300

    Completed implementation of post widget detail

--- app/mobile/lib/data/widgets/post_detail_widget.dart
+++ app/mobile/lib/data/widgets/post_detail_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mobile/data/constants/palette.dart';
 import 'package:mobile/data/models/post_model.dart';
+import '../../../data/constants/assets.dart';
 
 class PostDetailWidget extends StatelessWidget {
   final PostModel post;
@@ -18,6 +21,7 @@ class PostDetailWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -29,15 +33,30 @@ class PostDetailWidget extends StatelessWidget {
                   backgroundImage:
                       NetworkImage(getProfileImageById(post.userId)),
                 ),
+                SizedBox(
+                  width: 10,
+                ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(getIANameById(post.id)),
+                    Text(
+                      getIANameById(post.iaIds.first),
+                      style: TextStyle(
+                          color: Color(
+                            0xff9a208e,
+                          ),
+                          fontWeight: FontWeight.w600),
+                    ),
                     Text(getNameSurnameById(post.userId)),
-                    TextButton(
-                        onPressed: () {
-                          //Follow
-                        },
-                        child: const Text("Follow")),
+                    OutlinedButton(
+                      onPressed: () {
+                        //Follow
+                      },
+                      child: const Text("Follow"),
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    )
                   ],
                 ),
               ],
@@ -46,48 +65,122 @@ class PostDetailWidget extends StatelessWidget {
           ],
         ),
         Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(post.title),
-            Wrap(
-              children: [
-                //Labels
-              ],
+            Text(
+              post.title,
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
             ),
-            Expanded(child: Text(post.sourceLink)),
-            Expanded(
-              child: Text(post.content),
+            Wrap(
+              children: post.iaIds
+                  .map((e) => Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(12)),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                          child: Text(getIANameById(e)),
+                        ),
+                      ))
+                  .toList(),
+            ),
+            Text(
+              post.sourceLink,
+              style: TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              post.content,
             )
           ],
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
+        const SizedBox(
+          height: 10,
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Row(
               children: [
-                //Source
-                //Date
-                Row(
-                  children: [
-                    //Upvote
-                    //Downvote
-                    //Report button
-                  ],
-                ),
+                Text('Kaynak: '),
               ],
             ),
             Row(
               children: [
-                //Fact check
-                //Geolocation
+                Text('Paylaşım Tarihi: '),
               ],
+            ),
+          ]),
+          Row(
+            children: [
+              Image.asset(Assets.factCheck),
+              Text(
+                'UNKNOWN',
+                style: TextStyle(
+                    color: Color(0xffd9831f), fontWeight: FontWeight.w900),
+              )
+            ],
+          ),
+        ]),
+        Row(
+          children: [
+            Image.asset(Assets.upvote),
+            const SizedBox(
+              width: 5,
+            ),
+            Text(
+              '175',
+              style: TextStyle(color: Colors.green),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Image.asset(Assets.downvote),
+            const SizedBox(
+              width: 5,
+            ),
+            Text(
+              '18',
+              style: TextStyle(color: Colors.red),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            InkWell(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  "Report",
+                  style: TextStyle(color: Colors.white),
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0XFFBA1F1F),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
             ),
           ],
         ),
+        SizedBox(
+          height: 10,
+        ),
         Wrap(
-          children: [
-            //Tags
-          ],
-        )
+          children: post.iaIds
+              .map((e) => Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(12)),
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      child: Text(getIANameById(e)),
+                    ),
+                  ))
+              .toList(),
+        ),
       ],
     ));
   }
--- app/mobile/lib/modules/newIa/views/new_ia_view.dart
+++ app/mobile/lib/modules/newIa/views/new_ia_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:mobile/data/widgets/post_detail_widget.dart';
 
 import '../controllers/new_ia_controller.dart';
 
@@ -10,16 +11,22 @@ class NewIaView extends GetView<NewIaController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('NewIaView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'NewIaView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('NewIaView'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+          child: ListView.builder(
+              itemCount: controller.posts.length,
+              itemBuilder: (context, index) {
+                final post = controller.posts[index];
+                return PostDetailWidget(
+                    post: post,
+                    getProfileImageById: controller.getProfileImageById,
+                    getIANameById: controller.getIaNameById,
+                    getNameSurnameById: controller.getNameSurnameById);
+              }),
+        ));
   }
 }
