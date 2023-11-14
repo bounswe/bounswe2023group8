0c660b9e5752f6b46c81657932aa0599d73ea3cd
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-11-14 21:13:24 +0300

    add a new method in EntityTagsRepository

--- app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/EntityTagsRepository.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/EntityTagsRepository.java
@@ -1,9 +1,13 @@
 package com.wia.enigma.dal.repository;
 
-import com.wia.enigma.dal.entity.EntityTags;
+import com.wia.enigma.dal.entity.EntityTag;
+import com.wia.enigma.dal.enums.EntityType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EntityTagsRepository extends JpaRepository<EntityTags, Long> {
+import java.util.List;
 
+public interface EntityTagsRepository extends JpaRepository<EntityTag, Long> {
+
+    public List<EntityTag> findAllByEntityIdAndEntityType(Long entityId, EntityType entityType);
 
 }
