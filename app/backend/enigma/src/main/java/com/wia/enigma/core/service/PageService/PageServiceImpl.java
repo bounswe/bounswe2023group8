b88a858fa75f1048af31fe7ebdebb03b898c7c93
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-11-21 01:13:59 +0300

    create page service

---
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/service/PageService/PageServiceImpl.java
@@ -0,0 +1,39 @@
+package com.wia.enigma.core.service.PageService;
+
+import com.wia.enigma.core.data.dto.EnigmaUserDto;
+import com.wia.enigma.core.data.dto.ProfilePageDto;
+import com.wia.enigma.core.service.UserService.EnigmaUserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PageServiceImpl implements PageService{
+
+    final EnigmaUserService enigmaUserService;
+
+    public ProfilePageDto getProfilePage(Long userId, Long profileId) {
+
+        EnigmaUserDto enigmaUserDto = enigmaUserService.getVerifiedUser(userId);
+
+        ProfilePageDto profilePageDto = ProfilePageDto.builder()
+
+                .id(enigmaUserDto.getId())
+                .username(enigmaUserDto.getUsername())
+                .birthday(enigmaUserDto.getBirthday())
+                .followers(enigmaUserService.getFollowers(userId, profileId))
+                .following(enigmaUserService.getFollowings(userId, profileId))
+                .interestAreas(enigmaUserService.getFollowingInterestAreas(userId, profileId))
+                .posts(Collections.emptyList())
+                .build();
+
+        return profilePageDto;
+    }
+}
