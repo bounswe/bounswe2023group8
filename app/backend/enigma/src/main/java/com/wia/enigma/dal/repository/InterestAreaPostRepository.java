5f7d8b0d89a3f0532a8f0766d0c582a8304db9fe
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-11-23 02:07:36 +0300

    create InterestAreaPost model and repository

---
+++ app/backend/enigma/src/main/java/com/wia/enigma/dal/repository/InterestAreaPostRepository.java
@@ -0,0 +1,9 @@
+package com.wia.enigma.dal.repository;
+
+import com.wia.enigma.dal.entity.InterestAreaPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestAreaPostRepository extends JpaRepository<InterestAreaPost, Long> {
+
+
+}
