45481e9a799c574493d22d0b368d0883a65cb8eb
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-11-23 02:59:34 +0300

    refactor PostService

--- app/backend/enigma/src/main/java/com/wia/enigma/core/service/PostService/PostServiceImpl.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/service/PostService/PostServiceImpl.java
@@ -8,6 +8,7 @@ import com.wia.enigma.core.service.InterestAreaService.InterestAreaService;
 import com.wia.enigma.core.service.UserFollowsService.UserFollowsService;
 import com.wia.enigma.core.service.WikiService.WikiService;
 import com.wia.enigma.dal.entity.EntityTag;
+import com.wia.enigma.dal.entity.InterestAreaPost;
 import com.wia.enigma.dal.entity.Post;
 import com.wia.enigma.dal.enums.EntityType;
 import com.wia.enigma.dal.enums.ExceptionCodes;
@@ -25,6 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -41,136 +43,41 @@ public class PostServiceImpl implements PostService{
 
     @Override
     public PostDto getPost(Long postId, Long userId) {
+        Post post = fetchPost(postId);
+        validateUserFollowsInterestArea(userId, post);
 
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new EnigmaException(ExceptionCodes.ENTITY_NOT_FOUND, String.format("Post %d not found", postId)));
+        List<WikiTagDto> wikiTags = fetchWikiTagsForPost(post);
 
-
-        if(!userFollowsService.isUserFollowsEntity(userId, post.getInterestAreaId(), EntityType.INTEREST_AREA)){
-
-            throw new EnigmaException(ExceptionCodes.NON_AUTHORIZED_ACTION, String.format("User %d is not following interest area %d", userId, post.getInterestAreaId()));
-        }
-
-        List <WikiTagDto> wikiTags;
-        try{
-            wikiTags =  wikiTagService.getWikiTags(entityTagsRepository.findAllByEntityIdAndEntityType(post.getId(), EntityType.POST).stream().map(
-                    entityTags -> entityTags.getWikiDataTagId()).toList());
-        }catch(Exception e){
-            throw new EnigmaException(ExceptionCodes.INTERNAL_SERVER_ERROR, "Error occurred while fetching wiki tags.");
-        }
-
-
-        return PostDto.builder()
-                .id(post.getId())
-                .enigmaUserId(post.getEnigmaUserId())
-                .interestAreaId(post.getInterestAreaId())
-                .sourceLink(post.getSourceLink())
-                .title(post.getTitle())
-                .wikiTags(wikiTags)
-                .label(post.getLabel())
-                .geolocation(post.getGeolocation())
-                .createTime(post.getCreateTime())
-                .build();
+        return mapToPostDto(post, wikiTags);
     }
 
     @Override
     @Transactional
-    public PostDtoSimple createPost(Long userId, Long interestAreaId, String sourceLink, String title, List<String> wikiTags, PostLabel label, GeoLocation geolocation) {
+    public PostDtoSimple createPost(Long userId, Long interestAreaId, String sourceLink,
+                                    String title, List<String> wikiTags, PostLabel label,
+                                    GeoLocation geolocation) {
 
-        if(interestAreaService.isInterestAreaExist(interestAreaId) == false)
-            throw new EnigmaException(ExceptionCodes.INTEREST_AREA_NOT_FOUND, String.format("Interest area %d not found", interestAreaId));
+        validateInterestAreaAndUserFollowing(interestAreaId, userId);
+        Post post = createAndSavePost(userId, interestAreaId, sourceLink, title, label, geolocation);
+        saveWikiTagsForPost(post, wikiTags);
+        saveInterestAreaPost(interestAreaId, post);
 
-        if(!userFollowsService.isUserFollowsEntity(userId, interestAreaId, EntityType.INTEREST_AREA))
-            throw new EnigmaException(ExceptionCodes.NON_AUTHORIZED_ACTION, String.format("User %d is not following interest area %d", userId, interestAreaId));
-
-        Post post = Post.builder()
-                .enigmaUserId(userId)
-                .interestAreaId(interestAreaId)
-                .sourceLink(sourceLink)
-                .title(title)
-                .label(label)
-                .geolocation(geolocation)
-                .createTime(new Timestamp(System.currentTimeMillis()))
-                .build();
-
-        postRepository.save(post);
-
-        wikiTags.forEach(wikiTag -> {
-            entityTagsRepository.save(
-                    EntityTag.builder()
-                            .entityId(post.getId())
-                            .entityType(EntityType.POST)
-                            .wikiDataTagId(wikiTag)
-                            .createTime(new Timestamp(System.currentTimeMillis()))
-                            .build()
-            );
-        });
-
-
-        interestAreaPostRepository.save(
-                com.wia.enigma.dal.entity.InterestAreaPost.builder()
-                        .interestAreaId(interestAreaId)
-                        .postId(post.getId())
-                        .createTime(new Timestamp(System.currentTimeMillis()))
-                        .build()
-        );
-
-        return PostDtoSimple.builder()
-                .id(post.getId())
-                .enigmaUserId(post.getEnigmaUserId())
-                .interestAreaId(post.getInterestAreaId())
-                .sourceLink(post.getSourceLink())
-                .title(post.getTitle())
-                .wikiTags(wikiTags)
-                .label(post.getLabel())
-                .geolocation(post.getGeolocation())
-                .createTime(post.getCreateTime())
-                .build();
+        return mapToPostDtoSimple(post, wikiTags);
     }
 
     @Override
     @Transactional
-    public PostDtoSimple updatePost(Long userId, Long postId, String sourceLink, String title, List<String> wikiTags, PostLabel label, GeoLocation geolocation) {
+    public PostDtoSimple updatePost(Long userId, Long postId, String sourceLink,
+                                    String title, List<String> wikiTags,
+                                    PostLabel label, GeoLocation geolocation) {
 
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new EnigmaException(ExceptionCodes.ENTITY_NOT_FOUND, String.format("Post %d not found", postId)));
+        Post post = fetchPost(postId);
+        validatePostOwnership(userId, post);
 
-        if(post.getEnigmaUserId() != userId)
-            throw new EnigmaException(ExceptionCodes.NON_AUTHORIZED_ACTION, String.format("User %d is not the owner of post %d", userId, post.getId()));
+        updatePostDetails(post, sourceLink, title, label, geolocation);
+        updateWikiTagsForPost(post, wikiTags);
 
-
-        post.setSourceLink(sourceLink);
-        post.setTitle(title);
-        post.setLabel(label);
-        post.setGeolocation(geolocation);
-        post.setCreateTime(new Timestamp(System.currentTimeMillis()));
-
-        postRepository.save(post);
-
-        entityTagsRepository.deleteAllByEntityIdAndEntityType(post.getId(), EntityType.POST);
-
-        wikiTags.forEach(wikiTag -> {
-            entityTagsRepository.save(
-                    EntityTag.builder()
-                            .entityId(post.getId())
-                            .entityType(EntityType.POST)
-                            .wikiDataTagId(wikiTag)
-                            .createTime(new Timestamp(System.currentTimeMillis()))
-                            .build()
-            );
-        });
-
-        return PostDtoSimple.builder()
-                .id(post.getId())
-                .enigmaUserId(post.getEnigmaUserId())
-                .interestAreaId(post.getInterestAreaId())
-                .sourceLink(post.getSourceLink())
-                .title(post.getTitle())
-                .wikiTags(wikiTags)
-                .label(post.getLabel())
-                .geolocation(post.getGeolocation())
-                .createTime(post.getCreateTime())
-                .build();
+        return mapToPostDtoSimple(post, wikiTags);
     }
 
     @Override
@@ -185,5 +92,129 @@ public class PostServiceImpl implements PostService{
         }
 
         postRepository.delete(post);
+    }
+
+    private Post fetchPost(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new EnigmaException(ExceptionCodes.ENTITY_NOT_FOUND, String.format("Post %d not found", postId)));
+    }
+
+    private void validateUserFollowsInterestArea(Long userId, Post post) {
+        if (!userFollowsService.isUserFollowsEntity(userId, post.getInterestAreaId(), EntityType.INTEREST_AREA)) {
+            throw new EnigmaException(ExceptionCodes.NON_AUTHORIZED_ACTION, String.format("User %d is not following interest area %d", userId, post.getInterestAreaId()));
+        }
+    }
+
+    private List<WikiTagDto> fetchWikiTagsForPost(Post post) {
+        return wikiTagService.getWikiTags(
+                entityTagsRepository.findAllByEntityIdAndEntityType(post.getId(), EntityType.POST).stream()
+                        .map(EntityTag::getWikiDataTagId)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    private PostDto mapToPostDto(Post post, List<WikiTagDto> wikiTags) {
+        return PostDto.builder()
+                .id(post.getId())
+                .enigmaUserId(post.getEnigmaUserId())
+                .interestAreaId(post.getInterestAreaId())
+                .sourceLink(post.getSourceLink())
+                .title(post.getTitle())
+                .wikiTags(wikiTags)
+                .label(post.getLabel())
+                .geolocation(post.getGeolocation())
+                .createTime(post.getCreateTime())
+                .build();
+    }
+
+    private void validateInterestAreaAndUserFollowing(Long interestAreaId, Long userId) {
+        if (!interestAreaService.isInterestAreaExist(interestAreaId))
+            throw new EnigmaException(ExceptionCodes.INTEREST_AREA_NOT_FOUND, "Interest area " + interestAreaId + " not found");
+
+        if (!userFollowsService.isUserFollowsEntity(userId, interestAreaId, EntityType.INTEREST_AREA))
+            throw new EnigmaException(ExceptionCodes.NON_AUTHORIZED_ACTION, "User " + userId + " is not following interest area " + interestAreaId);
+    }
+
+    private Post createAndSavePost(Long userId, Long interestAreaId, String sourceLink,
+                                   String title, PostLabel label, GeoLocation geolocation) {
+        Post post = Post.builder()
+                .enigmaUserId(userId)
+                .interestAreaId(interestAreaId)
+                .sourceLink(sourceLink)
+                .title(title)
+                .label(label)
+                .geolocation(geolocation)
+                .createTime(new Timestamp(System.currentTimeMillis()))
+                .build();
+
+        return postRepository.save(post);
+    }
+
+    private void saveWikiTagsForPost(Post post, List<String> wikiTags) {
+        List<EntityTag> entityTags = wikiTags.stream().map(wikiTag ->
+                EntityTag.builder()
+                        .entityId(post.getId())
+                        .entityType(EntityType.POST)
+                        .wikiDataTagId(wikiTag)
+                        .createTime(new Timestamp(System.currentTimeMillis()))
+                        .build()
+        ).collect(Collectors.toList());
+
+        entityTagsRepository.saveAll(entityTags);
+    }
+
+    private void saveInterestAreaPost(Long interestAreaId, Post post) {
+        InterestAreaPost interestAreaPost = InterestAreaPost.builder()
+                .interestAreaId(interestAreaId)
+                .postId(post.getId())
+                .createTime(new Timestamp(System.currentTimeMillis()))
+                .build();
+
+        interestAreaPostRepository.save(interestAreaPost);
+    }
+
+    private PostDtoSimple mapToPostDtoSimple(Post post, List<String> wikiTags) {
+        return PostDtoSimple.builder()
+                .id(post.getId())
+                .enigmaUserId(post.getEnigmaUserId())
+                .interestAreaId(post.getInterestAreaId())
+                .sourceLink(post.getSourceLink())
+                .title(post.getTitle())
+                .wikiTags(wikiTags)
+                .label(post.getLabel())
+                .geolocation(post.getGeolocation())
+                .createTime(post.getCreateTime())
+                .build();
+    }
+
+    private void validatePostOwnership(Long userId, Post post) {
+        if (!post.getEnigmaUserId().equals(userId)) {
+            throw new EnigmaException(ExceptionCodes.NON_AUTHORIZED_ACTION, "User " + userId + " is not the owner of post " + post.getId());
+        }
+    }
+
+    private void updatePostDetails(Post post, String sourceLink, String title,
+                                   PostLabel label, GeoLocation geolocation) {
+        post.setSourceLink(sourceLink);
+        post.setTitle(title);
+        post.setLabel(label);
+        post.setGeolocation(geolocation);
+        post.setCreateTime(new Timestamp(System.currentTimeMillis()));
+        postRepository.save(post);
+    }
+
+    private void updateWikiTagsForPost(Post post, List<String> wikiTags) {
+        entityTagsRepository.deleteAllByEntityIdAndEntityType(post.getId(), EntityType.POST);
+
+        List<EntityTag> entityTags = wikiTags.stream().map(wikiTag ->
+                EntityTag.builder()
+                        .entityId(post.getId())
+                        .entityType(EntityType.POST)
+                        .wikiDataTagId(wikiTag)
+                        .createTime(new Timestamp(System.currentTimeMillis()))
+                        .build()
+        ).collect(Collectors.toList());
+
+        entityTagsRepository.saveAll(entityTags);
     }
 }
