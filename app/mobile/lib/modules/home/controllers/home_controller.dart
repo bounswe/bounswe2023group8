30e91f59819cda0d7a968998fb407e8bb834a7f3
celikomerfaruk <01omerfarukcelik@gmail.com>
2023-11-26 22:28:42 +0300

    home page api integrated

--- app/mobile/lib/modules/home/controllers/home_controller.dart
+++ app/mobile/lib/modules/home/controllers/home_controller.dart
@@ -1,18 +1,32 @@
 import 'package:get/get.dart';
+import 'package:mobile/data/helpers/error_handling_utils.dart';
 import 'package:mobile/data/models/spot.dart';
+import 'package:mobile/modules/bottom_navigation/controllers/bottom_navigation_controller.dart';
+import 'package:mobile/modules/home/providers/home_provider.dart';
 import '../../../routes/app_pages.dart';
 
 class HomeController extends GetxController {
   RxList<Spot> posts = <Spot>[].obs;
 
+  final routeLoading = true.obs;
 
+  final bottomNavigationController = Get.find<BottomNavigationController>();
+  final homeProvider = Get.find<HomeProvider>();
 
   void navigateToPostDetails(Spot post) {
     Get.toNamed(Routes.postDetails,
         arguments: {'post': post, 'visitor': false});
   }
 
-  void fetchData() {
+  void fetchData() async {
+    try {
+      posts.value = await homeProvider.getHomePage(
+              token: bottomNavigationController.token) ??
+          [];
+      routeLoading.value = false;
+    } catch (e) {
+      ErrorHandlingUtils.handleApiError(e);
+    }
   }
 
   @override
--- app/mobile/lib/modules/home/views/home_view.dart
+++ app/mobile/lib/modules/home/views/home_view.dart
@@ -1,22 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile/data/widgets/custom_search_bar.dart';
+import 'package:mobile/data/widgets/post_widget.dart';
 import 'package:mobile/modules/home/controllers/home_controller.dart';
 
 import '../../../data/widgets/custom_app_bar.dart';
-import 'member_view.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Obx(() {
+      if (controller.routeLoading.value) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+      return Scaffold(
         appBar: const CustomAppBar(
           leadingAppIcon: true,
           titleWidget: CustomSearchBar(),
         ),
-      body:  MemberView(),
+        body: Container(
+            color: Colors.white,
+            child: SingleChildScrollView(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: controller.posts.length,
+                      itemBuilder: (context, index) {
+                        return PostTileWidget(
+                          onTap: () => controller
+                              .navigateToPostDetails(controller.posts[index]),
+                          post: controller.posts[index],
+                          hideTags: false,
+                        );
+                      })
+                ],
+              ),
+            ))),
+      );
+    }
     );
   }
 }
