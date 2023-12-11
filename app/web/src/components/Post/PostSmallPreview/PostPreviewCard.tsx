ef29b0f323af5879640cb5d16eb3e84a805b70c4
EgemenKaplan <egemenkapln@gmail.com>
2023-12-12 00:45:59 +0300

    - Fixed styling of the PostPreviewCard.tsx component (Spot Preview) to comply with new mockup designs. - Added label component for displaying spot labels

--- app/web/src/components/Post/PostSmallPreview/PostPreviewCard.tsx
+++ app/web/src/components/Post/PostSmallPreview/PostPreviewCard.tsx
@@ -1,58 +1,104 @@
 import React from "react";
-import { format } from "date-fns";
+import {format, formatDistance} from "date-fns";
 import Tag from "../../Tag/Tag";
-import { Post } from "../../../pages/InterestAreaViewPage";
+import {Post} from "../../../pages/InterestAreaViewPage";
+import {Link} from "react-router-dom";
+import Label from "../../Label/Label";
 
 type PostPreviewCardProps = Post;
 
 const PostPreviewCard = ({
-  id,
-  enigmaUser,
-  interestArea,
-  sourceLink,
-  title,
-  wikiTags,
-  label,
-  content,
-  geolocation,
-  createTime,
-}: PostPreviewCardProps) => {
-  const createdAtString = format(new Date(createTime), "dd.MM.yyyy");
+                             id,
+                             enigmaUser,
+                             interestArea,
+                             sourceLink,
+                             title,
+                             wikiTags,
+                             label,
+                             content,
+                             geolocation,
+                             createTime,
+                         }: PostPreviewCardProps) => {
+    let createdAtString;
+    const createdDate = new Date(createTime);
+    if ((new Date().getTime() - createdDate.getTime()) / (1000 * 60 * 60 * 24) < 1) {
+        createdAtString = formatDistance(
+            createdDate,
+            new Date(),
+            {addSuffix: true}
+        )
+    } else {
+        createdAtString = format(new Date(createTime), "dd.MM.yyyy");
+    }
 
-  return (
-    <div className="card mb-3 bg-primary-subtle">
-      <div className="row g-0">
-        <div className="col-9">
-          <div className="card-body">
-            <a href={`/posts/${id}`}><h6 className="card-title truncate-text-4">{title}</h6></a>
-            <div className="card-title truncate-text-4">{content}</div>
-            <p>
-              <a href={sourceLink} className="link-primary">
-                {sourceLink}
-              </a>
-            </p>
-            <p className="card-text justify-content-between d-flex">
-              <small className="text-body-secondary">
-                Created by {enigmaUser.username}
-              </small>
-              <small className="text-body-secondary">{createdAtString}</small>
-            </p>
-          </div>
+    const likes =  Math.floor(Math.random() * (5 - (-5) + 1)) - 5;
+
+    return (
+        <div className="card WA-theme-bg-regular rounded-4 mb-3">
+            <div className="justify-content-between d-flex d-inline-flex">
+                <span className="d-flex">
+                    <span style={{position:"relative", top: '-0.5em'}}>
+                        <img alt="Bunch Icon" src="/assets/theme/images/Bunch.png"></img>
+                    </span>
+                    <span>
+                        <Link to={`/interest-area/${interestArea.id}`}
+                              style={{textDecoration: 'none'}}
+                              className="fs-4 fw-bold WA-theme-dark">
+                                {interestArea.title}
+                        </Link>
+                        <div className="WA-theme-dark" >Spotted by
+                            <Link to={`/profile/${enigmaUser.id}`}
+                                  style={{textDecoration: 'none'}}
+                                  className="WA-theme-main">
+                                {` @${enigmaUser.username}`}
+                            </Link>
+                            <i className="bi bi-dot fs-6"></i>
+                            <span>{` ${createdAtString}`}</span>
+                        </div>
+                    </span>
+                </span>
+                <span className="m-3">
+                    <img alt="upvote icon" src="/assets/theme/icons/Upvote.png" width="32" height="32" />
+                    {likes < 0
+                        ? <span className="WA-theme-negative fw-bold fs-6"> {-likes} </span>
+                        : <span className="WA-theme-positive fw-bold fs-6"> {likes} </span>
+                    }
+                    <img alt="downvote icon" src="/assets/theme/icons/Downvote.png" width="32" height="32"/>
+                </span>
+            </div>
+            <div className="card WA-theme-bg-light rounded-4 ms-4 m-1 ">
+                <img alt="Bookmark Icon" src="/assets/theme/images/FactCheck=False.png"
+                     width="64" height="64"
+                     style={{position: "absolute", top: '-0.66em', left: '-0.60em'}}
+                />
+                <div className="row g-0 ms-4">
+                    <div className="col-9">
+                        <div className="card-body">
+                            <Link to={`/posts/${id}`}
+                                  style={{textDecoration: 'none'}}
+                                  className="card-title truncate-text-2 WA-theme-dark fs-5 fw-bold">{title}</Link>
+                            <Label className="" label={label}/>
+                            <Link to={sourceLink} className="truncate-text-2 WA-theme-main fw-bold">
+                                {sourceLink}
+                            </Link>
+                            <div className="card-title truncate-text-4 WA-theme-dark">{content}</div>
+                        </div>
+                    </div>
+                    <div className="col-3 container d-flex justify-content-start p-1 m-0 ">
+                        <div className="vr my-3 col-1"></div>
+                        <div
+                            className="mx-auto flex-fill align-self-center overflow-y-auto"
+                            style={{maxHeight: "200px"}}
+                        >
+                            {wikiTags.map((tag: any) => (
+                                <Tag className="" key={`${id}-${tag.id}`} label={tag.label}/>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-3 container d-flex justify-content-start p-1 m-0 ">
-          <div className="vr my-3 col-1"></div>
-          <div
-            className="mx-auto align-self-center overflow-y-auto"
-            style={{ maxHeight: "200px" }}
-          >
-            {wikiTags.map((tag: any) => (
-              <Tag className={""} key={`${id}-${tag.id}`} label={tag.label} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PostPreviewCard;
