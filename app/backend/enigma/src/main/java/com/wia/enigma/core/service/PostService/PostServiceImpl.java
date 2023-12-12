72942931824460292f7505e0ccef827b3923b643
bahadirgezer <bahadirgezerbg@gmail.com>
2023-12-12 15:50:21 +0300

    merge resolution

--- app/backend/enigma/src/main/java/com/wia/enigma/core/service/PostService/PostServiceImpl.java
+++ app/backend/enigma/src/main/java/com/wia/enigma/core/service/PostService/PostServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
