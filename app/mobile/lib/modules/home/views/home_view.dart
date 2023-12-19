01278d8b815c5c516f1ba2d55c46571597ec08ad
Meriç Keskin <meric.keskin@external.yapikredi.com.tr>
2023-12-19 10:19:18 +0300

    Small fix

--- app/mobile/lib/modules/home/views/home_view.dart
+++ app/mobile/lib/modules/home/views/home_view.dart
@@ -16,164 +16,167 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      if (controller.routeLoading.value) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-      return Scaffold(
-        appBar: CustomAppBar(
-          leadingAppIcon: true,
-          titleWidget: CustomSearchBar(
-            onChanged: controller.onSearchQueryChanged,
+    return Obx(
+      () {
+        if (controller.routeLoading.value) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Scaffold(
+          appBar: CustomAppBar(
+            leadingAppIcon: true,
+            titleWidget: CustomSearchBar(
+              onChanged: controller.onSearchQueryChanged,
+            ),
+            actions: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 14.0, horizontal: 8.0),
+                child: SvgPicture.asset(Assets.notification),
+              )
+            ],
           ),
-          actions: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 14.0, horizontal: 8.0),
-              child: SvgPicture.asset(Assets.notification),
-            )
-          ],
-        ),
-        body: Container(
+          body: Container(
             color: ThemePalette.white,
             child: SingleChildScrollView(
-                child: controller.searchQuery.value.isNotEmpty
-                    ? _searchBody()
-                    : Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(right: 16),
-                            padding: const EdgeInsets.only(
-                                left: 16, top: 8, bottom: 8),
-                            decoration: BoxDecoration(
-                              borderRadius: const BorderRadius.only(
-                                  bottomRight: Radius.circular(10)),
-                              color: BackgroundPalette.regular,
-                            ),
-                            child: Row(
-                              children: [
-                                Text(
-                                  'Sort By:',
-                                  style: TextStyle(
-                                    color: ThemePalette.dark,
-                                    fontSize: 16,
-                                    fontFamily: 'Inter',
-                                    fontWeight: FontWeight.w600,
-                                    letterSpacing: -0.2,
-                                  ),
-                                ),
-                                const SizedBox(width: 12),
-                                InkWell(
-                                  onTap: () {},
-                                  child: Container(
-                                    height: 28,
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 4, vertical: 2),
-                                    decoration: BoxDecoration(
-                                      color: BackgroundPalette.soft,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        Image.asset(
-                                          Assets.sortNew,
-                                          width: 24,
-                                          height: 24,
-                                        ),
-                                        const SizedBox(width: 4),
-                                        Text(
-                                          "New",
-                                          style: TextStyle(
-                                            color: ThemePalette.dark,
-                                            fontSize: 12,
-                                            fontFamily: 'Inter',
-                                            fontWeight: FontWeight.w400,
-                                            letterSpacing: -0.15,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(width: 12),
-                                InkWell(
-                                  onTap: () {},
-                                  child: Container(
-                                    height: 28,
-                                    padding: const EdgeInsets.only(
-                                        left: 2, right: 4, top: 2, bottom: 2),
-                                    decoration: BoxDecoration(
-                                      color: BackgroundPalette.soft,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        Image.asset(
-                                          Assets.sortTop,
-                                          width: 24,
-                                          height: 24,
-                                        ),
-                                        const SizedBox(width: 2),
-                                        Text(
-                                          "Top",
-                                          style: TextStyle(
-                                            color: ThemePalette.dark,
-                                            fontSize: 12,
-                                            fontFamily: 'Inter',
-                                            fontWeight: FontWeight.w400,
-                                            letterSpacing: -0.15,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+              child: controller.searchQuery.value.isNotEmpty
+                  ? _searchBody()
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(right: 16),
+                          padding: const EdgeInsets.only(
+                              left: 16, top: 8, bottom: 8),
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.only(
+                                bottomRight: Radius.circular(10)),
+                            color: BackgroundPalette.regular,
                           ),
-                          ListView.builder(
-                              shrinkWrap: true,
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 16),
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount: controller.posts.length,
-                              itemBuilder: (context, index) {
-                                return Column(
-                                  children: [
-                                    const SizedBox(height: 8),
-                                    PostTileWidget(
-                                      onTap: () =>
-                                          controller.navigateToPostDetails(
-                                              controller.posts[index]),
-                                      post: controller.posts[index],
-                                      hideTags: false,
-                                      onUpvote: () => controller.upvotePost(
+                          child: Row(
+                            children: [
+                              Text(
+                                'Sort By:',
+                                style: TextStyle(
+                                  color: ThemePalette.dark,
+                                  fontSize: 16,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: -0.2,
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  height: 28,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 4, vertical: 2),
+                                  decoration: BoxDecoration(
+                                    color: BackgroundPalette.soft,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Image.asset(
+                                        Assets.sortNew,
+                                        width: 24,
+                                        height: 24,
+                                      ),
+                                      const SizedBox(width: 4),
+                                      Text(
+                                        "New",
+                                        style: TextStyle(
+                                          color: ThemePalette.dark,
+                                          fontSize: 12,
+                                          fontFamily: 'Inter',
+                                          fontWeight: FontWeight.w400,
+                                          letterSpacing: -0.15,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  height: 28,
+                                  padding: const EdgeInsets.only(
+                                      left: 2, right: 4, top: 2, bottom: 2),
+                                  decoration: BoxDecoration(
+                                    color: BackgroundPalette.soft,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Image.asset(
+                                        Assets.sortTop,
+                                        width: 24,
+                                        height: 24,
+                                      ),
+                                      const SizedBox(width: 2),
+                                      Text(
+                                        "Top",
+                                        style: TextStyle(
+                                          color: ThemePalette.dark,
+                                          fontSize: 12,
+                                          fontFamily: 'Inter',
+                                          fontWeight: FontWeight.w400,
+                                          letterSpacing: -0.15,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: controller.posts.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                const SizedBox(height: 8),
+                                PostTileWidget(
+                                  onTap: () => controller.navigateToPostDetails(
+                                      controller.posts[index]),
+                                  post: controller.posts[index],
+                                  hideTags: false,
+                                  onUpvote: () => controller
+                                      .upvotePost(controller.posts[index].id),
+                                  onDownvote: () => controller
+                                      .downvotePost(controller.posts[index].id),
+                                  showDownvoters: () =>
+                                      controller.showDownVotes(
                                           controller.posts[index].id),
-                                      onDownvote: () => controller.downvotePost(
-                                          controller.posts[index].id),
-                                      showDownvoters: () =>
-                                          controller.showDownVotes(
-                                              controller.posts[index].id),
-                                      showUpvoters: () =>
-                                          controller.showUpVotes(
-                                              controller.posts[index].id),
-                                    )
-                                  ],
-                                );
-                              })
-                        ],
-                      ))),
-      );
-    });
+                                  showUpvoters: () => controller
+                                      .showUpVotes(controller.posts[index].id),
+                                )
+                              ],
+                            );
+                          },
+                        )
+                      ],
+                    ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   Widget _searchBody() {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           if (controller.searchIas.isNotEmpty) ...[
@@ -196,6 +199,7 @@ class HomeView extends GetView<HomeController> {
             const SizedBox(height: 8),
             ListView.separated(
               shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
               itemCount: controller.searchIas.length,
               itemBuilder: (context, index) {
                 final ia = controller.searchIas[index];
@@ -283,12 +287,12 @@ class HomeView extends GetView<HomeController> {
                         .navigateToPostDetails(controller.searchPosts[index]),
                     post: controller.searchPosts[index],
                     hideTags: false,
-					onDownvote: () =>
-                      controller.downvotePost(controller.searchPosts[index].id),
-                  onUpvote: () =>
-                      controller.upvotePost(controller.searchPosts[index].id),
-                  showDownvoters: () {},
-                  showUpvoters: () {},
+                    onDownvote: () => controller
+                        .downvotePost(controller.searchPosts[index].id),
+                    onUpvote: () =>
+                        controller.upvotePost(controller.searchPosts[index].id),
+                    showDownvoters: () {},
+                    showUpvoters: () {},
                   );
                 },
                 separatorBuilder: (context, index) {
