10504e092896bedd1dc3e2553b087ff3ffe70808
Bahri Alabey <alabeybb@gmail.com>
2023-12-21 17:12:03 +0300

    add user report function and fix layout

--- app/web/src/components/ProfileHeader/ProfileHeader.tsx
+++ app/web/src/components/ProfileHeader/ProfileHeader.tsx
@@ -1,82 +1,168 @@
-import React, {useState} from 'react';
-import {Col, Row} from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import SpanWithOnClick from "../shared/SpanWithOnClick/SpanWithOnClick";
 import FollowerModal from "../Follow/Follower/FollowerModal";
 import FollowingModal from "../Follow/Following/FollowingModal";
+import { useReportAnIssue } from "../../hooks/useModeration";
+import { useAuth } from "../../contexts/AuthContext";
 
 type ProfileHeaderProps = {
-    user: {
-        id: number,
-        name: string,
-        username: string,
-        followers: number,
-        following: number,
-        user_profile_image: string
-    },
-    style: object,
-    className: string
+  user: {
+    id: number;
+    name: string;
+    username: string;
+    followers: number;
+    following: number;
+    user_profile_image: string;
+  };
+  style: object;
+  className: string;
 };
 
-
 const ProfileHeader = ({
-                           user: {
-                               id,
-                               name,
-                               username,
-                               followers,
-                               following,
-                               user_profile_image,
-                           },
-                           style,
-                           className
-                       }: ProfileHeaderProps) => {
-    const [showFollowerModal, setShowFollowerModal] = useState(false);
-    const [showFollowingModal, setShowFollowingModal] = useState(false);
+  user: { id, name, username, followers, following, user_profile_image },
+  style,
+  className,
+}: ProfileHeaderProps) => {
+  const [showFollowerModal, setShowFollowerModal] = useState(false);
+  const [showFollowingModal, setShowFollowingModal] = useState(false);
+  const [reportReason, setReportReason] = useState("");
+  const [showReport, setShowReport] = useState(false);
 
-    const handleFollowerModal = () =>{
-        setShowFollowerModal(!showFollowerModal);
-    }
+  const { axiosInstance } = useAuth();
 
+  const { mutate: reportAnIssue } = useReportAnIssue({
+    axiosInstance,
+    issue: {
+      entityId: id,
+      entityType: "user",
+      reason: reportReason,
+    },
+  });
 
-    const handleFollowingModal = () =>{
-        setShowFollowingModal(!showFollowingModal);
-    }
+  const reportUser = () => {
+    reportAnIssue({
+      axiosInstance,
+      issue: {
+        entityId: id,
+        entityType: "user",
+        reason: reportReason,
+      },
+    });
+  };
 
-    return <div className={`card mt-3 mb-1 ${className}`} style={style}>
+  const handleFollowerModal = () => {
+    setShowFollowerModal(!showFollowerModal);
+  };
+
+  const handleFollowingModal = () => {
+    setShowFollowingModal(!showFollowingModal);
+  };
+
+  return (
+    <div
+      style={{
+        background: "#EEF0EB",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <div className={`card mt-3 mb-1 ${className}`} style={style}>
         <Row className="g-0">
-            <Col className="col-4 justify-content-center my-4" style={{maxHeight: '80px', maxWidth: '80px'}}>
-                {user_profile_image
-                    ? <img src={user_profile_image} className="rounded-circle img-fluid object-fit-cover h-100 w-100"
-                     style={{borderRadius: '50%'}} alt="PP goes here"/>
-                    : <h1 className="bi bi-person-circle display-1 mx-4"></h1>}
-            </Col>
-            <Col className="col-8">
-                <Row className="card-body">
-                    <Col className="col-10 m-auto">
-                    <h5 className="card-title" style={{ fontSize: '2rem' }}>{name}</h5>
-                    <p className="card-text text-body-secondary" style={{ fontSize: '1.5rem' }}>@{username}</p>
+          <Col
+            className="col-4 justify-content-center my-4"
+            style={{ maxHeight: "80px", maxWidth: "80px" }}
+          >
+            {user_profile_image ? (
+              <img
+                src={user_profile_image}
+                className="rounded-circle img-fluid object-fit-cover h-100 w-100"
+                style={{ borderRadius: "50%" }}
+                alt="PP goes here"
+              />
+            ) : (
+              <h1 className="bi bi-person-circle display-1 mx-4"></h1>
+            )}
+          </Col>
+          <Col className="col-8">
+            <Row className="card-body">
+              <Col className="col-10 m-auto">
+                <h5 className="card-title" style={{ fontSize: "2rem" }}>
+                  {name}
+                </h5>
+                <p
+                  className="card-text text-body-secondary"
+                  style={{ fontSize: "1.5rem" }}
+                >
+                  @{username}
+                </p>
 
-                        <Row className="justify-content-between">
-                            <Col className="card-text" style={{ fontSize: '1.2rem' }}>
-                                <SpanWithOnClick className={""} text={`${followers} followers`}
-                                                 onClick={handleFollowerModal}/>
-                            </Col>
-                            <Col className="card-text" style={{ fontSize: '1.2rem'}}>
-                                <SpanWithOnClick className={""} text={`${following} following`}
-                                                 onClick={handleFollowingModal}/>
-                            </Col>
-                        </Row>
-                    </Col>
-                    {/*<Col className="col-2 m-auto">*/}
-                    {/*    <p className="fs-5 bi-hand-thumbs-up-fill " style={{color: 'green'}}>{all_time_likes}</p>*/}
-                    {/*    <p className="fs-5 bi-hand-thumbs-down-fill text-danger">{all_time_dislikes}</p>*/}
-                    {/*</Col>*/}
+                <Row className="justify-content-between">
+                  <Col className="card-text" style={{ fontSize: "1.2rem" }}>
+                    <SpanWithOnClick
+                      className={""}
+                      text={`${followers} followers`}
+                      onClick={handleFollowerModal}
+                    />
+                  </Col>
+                  <Col className="card-text" style={{ fontSize: "1.2rem" }}>
+                    <SpanWithOnClick
+                      className={""}
+                      text={`${following} following`}
+                      onClick={handleFollowingModal}
+                    />
+                  </Col>
                 </Row>
-            </Col>
+              </Col>
+              {/*<Col className="col-2 m-auto">*/}
+              {/*    <p className="fs-5 bi-hand-thumbs-up-fill " style={{color: 'green'}}>{all_time_likes}</p>*/}
+              {/*    <p className="fs-5 bi-hand-thumbs-down-fill text-danger">{all_time_dislikes}</p>*/}
+              {/*</Col>*/}
+            </Row>
+          </Col>
         </Row>
-        <FollowerModal show={showFollowerModal} setShow={handleFollowerModal}/>
-        <FollowingModal show={showFollowingModal} setShow={handleFollowingModal}/>
+        <FollowerModal show={showFollowerModal} setShow={handleFollowerModal} />
+        <FollowingModal
+          show={showFollowingModal}
+          setShow={handleFollowingModal}
+        />
+      </div>
+      <Col>
+        <div className="mx-2 my-3 w-75 WA-theme-bg-light d-flex justify-content-center align-items-center rounded-5">
+          {showReport ? (
+            <>
+              <input
+                type="text"
+                className="form-control m-4 "
+                placeholder="Please write a reason"
+                onChange={(e) => setReportReason(e.target.value)}
+              ></input>
+              <div className="d-flex mx-3">
+                <button onClick={() => reportUser()} className="btn">
+                  Submit
+                </button>
+                <button
+                  onClick={() => setShowReport(!showReport)}
+                  className="btn"
+                >
+                  Close
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => setShowReport(!showReport)}
+                className="btn mx-3 rounded-5"
+              >
+                Report
+              </button>
+            </>
+          )}
+        </div>
+      </Col>
     </div>
-}
+  );
+};
 
 export default ProfileHeader;
--- app/web/src/pages/ProfilePage/index.tsx
+++ app/web/src/pages/ProfilePage/index.tsx
@@ -1,83 +1,113 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import PostPreviewCard from "../../components/Post/PostSmallPreview/PostPreviewCard";
-import InterestAreaCard from '../../components/InterestArea/InterestAreaCard';
-import {Col, Row} from "react-bootstrap";
+import InterestAreaCard from "../../components/InterestArea/InterestAreaCard";
+import { Col, Row } from "react-bootstrap";
 import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
-import {useAuth} from "../../contexts/AuthContext";
-import {useGetUserFollowingInterestAreas, useGetUserPosts, useGetUserProfile} from "../../hooks/useProfile";
-import {useParams} from "react-router-dom";
-import {Post} from "../InterestAreaViewPage";
+import { useAuth } from "../../contexts/AuthContext";
+import {
+  useGetUserFollowingInterestAreas,
+  useGetUserPosts,
+  useGetUserProfile,
+} from "../../hooks/useProfile";
+import { useParams } from "react-router-dom";
+import { Post } from "../InterestAreaViewPage";
 
 const ProfilePage = () => {
+  const { userId } = useParams();
 
-    const {userId} = useParams();
+  const { axiosInstance } = useAuth();
+  const {
+    mutate: getUserProfile,
+    data: profileData,
+    isSuccess: isSuccessProfile,
+  } = useGetUserProfile({});
+  const {
+    mutate: getUserFollowingInterestAreas,
+    data: interestAreas,
+    isSuccess: isSuccessInterestAreas,
+  } = useGetUserFollowingInterestAreas({});
+  const {
+    mutate: getUserPosts,
+    data: posts,
+    isSuccess: isSuccessPosts,
+  } = useGetUserPosts({});
 
-    const {axiosInstance} = useAuth();
-    const {mutate: getUserProfile, data: profileData, isSuccess: isSuccessProfile} = useGetUserProfile({});
-    const {
-        mutate: getUserFollowingInterestAreas,
-        data: interestAreas,
-        isSuccess: isSuccessInterestAreas
-    } = useGetUserFollowingInterestAreas({});
-    const {mutate: getUserPosts, data: posts, isSuccess: isSuccessPosts} = useGetUserPosts({});
+  useEffect(() => {
+    getUserProfile({
+      axiosInstance: axiosInstance,
+      userId: userId || "-1",
+    });
+    console.log(profileData);
+    getUserFollowingInterestAreas({
+      axiosInstance: axiosInstance,
+      userId: userId || "-1",
+    });
+    getUserPosts({
+      axiosInstance: axiosInstance,
+      userId: userId || "-1",
+    });
+  }, []);
 
-    useEffect(() => {
-        getUserProfile({
-            axiosInstance: axiosInstance,
-            userId: userId || "-1",
-        })
-        console.log(profileData);
-        getUserFollowingInterestAreas({
-            axiosInstance: axiosInstance,
-            userId: userId || "-1",
-        })
-        getUserPosts({
-            axiosInstance: axiosInstance,
-            userId: userId || "-1",
-        })
-    }, [])
+  return (
+    <>
+      {isSuccessProfile && (
+        <ProfileHeader
+          style={{ background: "#EEF0EB", marginLeft: "60px" }}
+          user={profileData}
+          className="col-5 border-0"
+        />
+      )}
+      <hr className="mx-3" />
+      <Row>
+        <Col className="col-4">
+          <h5 className="mt-2 mx-3">Interest Areas</h5>
+          <div className="card border-0" style={{ maxHeight: "70vh" }}>
+            <hr className="m-0 mx-2" />
+            {isSuccessInterestAreas && (
+              <div className="card-body overflow-y-auto">
+                {interestAreas.map((interestArea: any) => (
+                  <InterestAreaCard
+                    key={interestArea.id}
+                    interestArea={interestArea}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+        </Col>
 
-
-    return <>
-        <div style={{height: "25%", background: "#EEF0EB", display: "flex", alignItems: "center", margin: "0px", padding: "0px"}}>
-        
-            {isSuccessProfile && <ProfileHeader style={{ background: "#EEF0EB", marginLeft: "60px" }} user={profileData} className="col-5 border-0"/>}
-        
-        </div>
-        <hr className="mx-3"/>
-        <Row>
-            <Col className="col-4">
-                <h5 className="mt-2 mx-3">Interest Areas</h5>
-                <div className="card border-0" style={{maxHeight: '70vh'}}>
-                    <hr className="m-0 mx-2"/>
-                    {isSuccessInterestAreas && <div className="card-body overflow-y-auto">
-                        {interestAreas.map((interestArea: any) => (
-                            <InterestAreaCard key={interestArea.id} interestArea={interestArea}/>
-                        ))}
-                    </div>}
-                </div>
-            </Col>
-
-            <Col className="col-8">
-                <h5 className="mt-2 mx-3">Posts</h5>
-                <div className="card border-0" style={{maxHeight: '70vh'}}>
-                    <hr className="m-0 mx-2"/>
-                    {isSuccessPosts && <div className="card-body overflow-y-auto">
-                        {posts.map((post: Post) => {
-                            return <PostPreviewCard
-                                key={post.id} content={post.content}
-                                createTime={post.createTime} enigmaUser={post.enigmaUser}
-                                geolocation={post.geolocation} id={post.id} interestArea={post.interestArea}
-                                wikiTags={post.wikiTags} label={post.label} title={post.title}
-                                sourceLink={post.sourceLink} upvoteCount={post.upvoteCount} downvoteCount={post.downvoteCount}/>
-                        })
-                        }
-                    </div>}
-                </div>
-            </Col>
-        </Row>
+        <Col className="col-8">
+          <h5 className="mt-2 mx-3">Posts</h5>
+          <div className="card border-0" style={{ maxHeight: "70vh" }}>
+            <hr className="m-0 mx-2" />
+            {isSuccessPosts && (
+              <div className="card-body overflow-y-auto">
+                {posts.map((post: Post) => {
+                  return (
+                    <PostPreviewCard
+                      key={post.id}
+                      content={post.content}
+                      createTime={post.createTime}
+                      enigmaUser={post.enigmaUser}
+                      geolocation={post.geolocation}
+                      id={post.id}
+                      interestArea={post.interestArea}
+                      wikiTags={post.wikiTags}
+                      label={post.label}
+                      title={post.title}
+                      sourceLink={post.sourceLink}
+                      upvoteCount={post.upvoteCount}
+                      downvoteCount={post.downvoteCount}
+                    />
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </Col>
+      </Row>
     </>
-        ;
+  );
 };
 
 export default ProfilePage;
