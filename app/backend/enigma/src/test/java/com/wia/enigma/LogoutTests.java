a7c84718a27d8818617fb8df41df3de25df41547
bahadirgezer <bahadirgezerbg@gmail.com>
2023-12-24 15:54:53 +0300

    fix enigma project signup, signin and logout tests

--- app/backend/enigma/src/test/java/com/wia/enigma/LogoutTests.java
+++ app/backend/enigma/src/test/java/com/wia/enigma/LogoutTests.java
@@ -1,6 +1,8 @@
 package com.wia.enigma;
 
 import com.wia.enigma.core.service.UserService.EnigmaUserService;
+import com.wia.enigma.dal.enums.ExceptionCodes;
+import com.wia.enigma.exceptions.custom.EnigmaException;
 import com.wia.enigma.utilities.JwtUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,14 +13,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.HeaderResultMatchers;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class LogoutTests {
 
@@ -28,17 +31,39 @@ public class LogoutTests {
     @MockBean
     private EnigmaUserService enigmaUserService;
 
-    @MockBean
-    private JwtUtils jwtUtils;
-
     @Test
-    public void testLogout_ValidJwt_ShouldReturnOk() throws Exception {
-        when(jwtUtils.getTokenType()).thenReturn("Bearer");
-        when(jwtUtils.assertValidJwt(anyString())).thenReturn("123456"); // Mocking a valid JTI return
+    public void logout_invalidHeader() throws Exception {
 
-        mockMvc.perform(post("/auth/logout")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer validJwtToken123"))
-                .andExpect(status().isOk());
+        String authorizationHeader = "NotBearer someJwt";
+        enigmaUserService.logoutEnigmaUser(authorizationHeader);
+
+        doThrow(new EnigmaException(ExceptionCodes.INVALID_AUTHORIZATION_HEADER, "Invalid authorization header."))
+                .when(enigmaUserService).logoutEnigmaUser(authorizationHeader);
+
+        mockMvc.perform(post("/api/auth/logout")
+                        .header(HttpHeaders.AUTHORIZATION, authorizationHeader))
+                .andExpect(status().isUnauthorized());
     }
 
+    @Test
+    public void logout_invalidJwt() throws Exception {
+
+        String authorizationHeader = "Bearer invalidJwt";
+        enigmaUserService.logoutEnigmaUser(authorizationHeader);
+
+        doThrow(new EnigmaException(ExceptionCodes.INVALID_JWT, "Invalid JWT."))
+                .when(enigmaUserService).logoutEnigmaUser(authorizationHeader);
+
+        mockMvc.perform(post("/api/auth/logout")
+                        .header(HttpHeaders.AUTHORIZATION, authorizationHeader))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void logout_success() throws Exception {
+
+        mockMvc.perform(post("/api/auth/logout")
+                        .header(HttpHeaders.AUTHORIZATION, anyString()))
+                .andExpect(status().isOk());
+    }
 }
--- app/backend/enigma/src/test/java/com/wia/enigma/SignupTests.java
+++ app/backend/enigma/src/test/java/com/wia/enigma/SignupTests.java
@@ -1,5 +1,7 @@
 package com.wia.enigma;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wia.enigma.core.data.request.SignupRequest;
 import com.wia.enigma.core.data.response.RegisterResponse;
 import com.wia.enigma.core.service.UserService.EnigmaUserService;
 import com.wia.enigma.dal.enums.ExceptionCodes;
@@ -13,16 +15,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class SignupTests {
 
@@ -33,43 +33,83 @@ public class SignupTests {
     private EnigmaUserService enigmaUserService;
 
     @Test
-    public void testSignup_ValidRequest_ShouldReturnCreated() throws Exception {
-        when(enigmaUserService.registerEnigmaUser(anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(new RegisterResponse(1L, false));
+    public void signup_success() throws Exception {
 
-        mockMvc.perform(post("/auth/signup")
+        RegisterResponse mockRegisterResponse = new RegisterResponse(1L, false);
+        when(enigmaUserService.registerEnigmaUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(mockRegisterResponse);
+
+        SignupRequest signupRequest = new SignupRequest(
+                "username",
+                "name",
+                "email@example.com",
+                "password",
+                "2000-01-01"
+        );
+
+        mockMvc.perform(post("/api/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\":\"testUsername\",\"email\":\"test@email.com\",\"password\":\"testPassword\",\"birthday\":\"2000-01-01\"}"))
-                .andExpect(status().isCreated());
+                        .content(asJsonString(signupRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", containsString("/api/v1/user/1")));
     }
 
     @Test
-    public void testSignup_UsernameContainsAtSymbol_ShouldReturnBadRequest() throws Exception {
+    public void signup_existingUsername() throws Exception {
 
-        String requestPayload = "{\"username\":\"test@Username\",\"email\":\"test@email.com\",\"password\":\"testPassword\",\"birthday\":\"2000-01-01\"}";
+        doThrow(new EnigmaException(ExceptionCodes.DB_UNIQUE_CONSTRAINT_VIOLATION,
+                "EnigmaUser with username or email already exists."))
+                .when(enigmaUserService).registerEnigmaUser(anyString(), anyString(), anyString(), anyString(), anyString());
 
-        doThrow(new EnigmaException(ExceptionCodes.INVALID_USERNAME, "Username cannot contain '@'."))
-                .when(enigmaUserService).registerEnigmaUser(eq("test@Username"), anyString(), anyString(), anyString());
+        SignupRequest signupRequest = new SignupRequest(
+                "existingUsername",
+                "name",
+                "email@example.com",
+                "password",
+                "2000-01-01"
+        );
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/auth/signup")
+        mockMvc.perform(post("/api/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestPayload))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.content().string(containsString("Username cannot contain '@'.")));
-    }
-
-    @Test
-    public void testSignup_InvalidBirthdayFormat_ShouldReturnBadRequest() throws Exception {
-        String invalidBirthdayPayload = "{\"username\":\"testUsername\",\"email\":\"test@email.com\",\"password\":\"testPassword\",\"birthday\":\"invalidDate\"}";
-
-        doThrow(new EnigmaException(ExceptionCodes.INVALID_DATE_FORMAT, "Birthday is not in the correct format. Use \"yyyy-[m]m-[d]d\""))
-                .when(enigmaUserService).registerEnigmaUser(anyString(), anyString(), anyString(), anyString());
-
-        mockMvc.perform(post("/auth/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(invalidBirthdayPayload))
+                        .content(asJsonString(signupRequest)))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string(containsString("Birthday is not in the correct format".trim())));
+                .andExpect(content().string(containsString("EnigmaUser with username or email already exists.")));
     }
 
+    @Test
+    public void signup_badEmailUsername() throws Exception {
+
+        SignupRequest signupRequest = new SignupRequest(
+                "username@",
+                "name",
+                "badEmail",
+                "password",
+                "2000-01-01"
+        );
+
+        doThrow(new EnigmaException(ExceptionCodes.INVALID_USERNAME,
+                "Username cannot contain '@'."))
+                .when(enigmaUserService)
+                .registerEnigmaUser(
+                        "username@",
+                        "name",
+                        "badEmail",
+                        "password",
+                        "2000-01-01"
+                );
+
+        mockMvc.perform(post("/api/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(signupRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("Username cannot contain '@'.")));
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
