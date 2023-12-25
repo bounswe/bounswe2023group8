63d4b9187db948ce9d463a421f35ce658667c992
baki-kucukcakiroglu <baki.kucukcakiroglu@trailblu.com>
2023-12-25 22:01:26 +0300

    cors origin pattern change

--- app/backend/annotation/src/main/java/com/wia/annotation/configuration/WebConfig.java
+++ app/backend/annotation/src/main/java/com/wia/annotation/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
